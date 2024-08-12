9f812d7a79be66c9b213c4a0333e64f10c05c9f0
Randy Creasi <rcreasi@gmail.com>
2024-08-12 10:18:52 -0400

    restore previous search query when feature selector is focused (#29)

--- frontend/packages/@depmap/data-explorer-2/src/components/DimensionSelect/EntitySelect/EntitySelect.tsx
+++ frontend/packages/@depmap/data-explorer-2/src/components/DimensionSelect/EntitySelect/EntitySelect.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useMemo } from "react";
+import React, { useCallback, useEffect, useMemo, useRef } from "react";
 import VanillaAsycSelect from "react-select/async";
 import { WindowedMenuList } from "react-windowed-select";
 import { DataExplorerContext } from "@depmap/types";
@@ -36,6 +36,11 @@ function EntitySelect({
   swatchColor = undefined,
 }: Props) {
   const search = useSearch();
+  const searchQuery = useRef("");
+
+  useEffect(() => {
+    searchQuery.current = "";
+  }, [entity_type, dataType, dataset_id]);
 
   const {
     aliases,
@@ -144,6 +149,10 @@ function EntitySelect({
       swatchColor={swatchColor}
       isClearable
       isEditable
+      editableInputValue={searchQuery.current}
+      onEditInputValue={(editedText) => {
+        searchQuery.current = editedText;
+      }}
     />
   );
 }
