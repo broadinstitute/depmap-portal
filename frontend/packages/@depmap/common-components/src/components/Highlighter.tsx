e4c076456d910c808d02ee5c92fab2e6ce89cbff
Randy Creasi <rcreasi@broadinstitute.org>
2024-08-15 17:09:08 -0400

    improve feature search highlighting (#34)

--- frontend/packages/@depmap/common-components/src/components/Highlighter.tsx
+++ frontend/packages/@depmap/common-components/src/components/Highlighter.tsx
@@ -3,12 +3,23 @@ import WordBreaker from "./WordBreaker";
 
 interface Props {
   text: string;
-  termToHiglight: string;
+  termToHiglight: string | string[];
   style?: React.CSSProperties;
   // Set this to `true` if you want highlight within words (rather than exactly
   // matching words)
   matchPartialTerms?: boolean;
 }
+
+export const colors = [
+  "yellow",
+  "#FF8152",
+  "#32CD32",
+  "#03A9F4",
+  "#DF9EEA",
+  "#FFA500",
+  "#FF6347",
+  "#D1E7B7",
+];
 
 // Wraps `termToHiglight` in a <span> with a yellow background. Also breaks
 // words just for good measure.
@@ -22,41 +33,57 @@ function Highlighter({
     return null;
   }
 
-  if (!termToHiglight) {
+  if (!termToHiglight || !termToHiglight.length) {
     return text;
   }
 
-  const escapedTerm = termToHiglight.replace(
-    // See https://stackoverflow.com/a/9310752
-    /[-[\]{}()*+?.,\\^$|#\s]/g,
-    "\\$&"
-  );
+  const terms = Array.isArray(termToHiglight)
+    ? termToHiglight
+    : [termToHiglight];
 
-  const regex = matchPartialTerms
-    ? RegExp(`${escapedTerm}`, "gi")
-    : RegExp(`\\b${escapedTerm}\\b`, "gi");
-
-  const elements: React.ReactNode[] = [];
-  const matches = text.match(regex);
-
-  text.split(regex).forEach((str, i) => {
-    // eslint-disable-next-line react/no-array-index-key
-    elements.push(<WordBreaker key={i} text={str} />);
-
-    if (matches && i < matches.length) {
-      elements.push(
-        <span
-          // eslint-disable-next-line react/no-array-index-key
-          key={`${i}-h`}
-          style={{ ...style, backgroundColor: "yellow" }}
-        >
-          <WordBreaker text={matches[i]} />
-        </span>
-      );
-    }
+  const escapedTerms = terms.map((term) => {
+    return term.replace(
+      // See https://stackoverflow.com/a/9310752
+      /[-[\]{}()*+?.,\\^$|#\s]/g,
+      "\\$&"
+    );
   });
 
-  return <>{elements}</>;
+  const regexPattern = matchPartialTerms
+    ? `(${escapedTerms.join("|")})`
+    : `\\b(${escapedTerms.join("|")})\\b`;
+
+  const regex = new RegExp(regexPattern, "gi");
+  const parts = text.split(regex);
+
+  const getColor = (part: string) => {
+    const index =
+      terms.findIndex(
+        (term: string) => part.toLowerCase() === term.toLowerCase()
+      ) || 0;
+
+    return colors[index % colors.length];
+  };
+
+  return (
+    <>
+      {parts.map((part, i) =>
+        terms.some((term) => part.toLowerCase() === term.toLowerCase()) ? (
+          <span
+            // eslint-disable-next-line react/no-array-index-key
+            key={`${i}-h`}
+            style={{ ...style, backgroundColor: getColor(part) }}
+          >
+            <WordBreaker text={part} />
+          </span>
+        ) : (
+          part
+        )
+      )}
+    </>
+  );
 }
+
+Highlighter.colors = colors;
 
 export default Highlighter;
