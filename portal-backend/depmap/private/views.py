5784d5fc2c58309ca8ca3c65d78d711266032609
Jessica Cheng <jcheng@broadinstitute.org>
2024-09-03 09:54:43 -0400

    Add support for downloading objects in directories within bucket

--- portal-backend/depmap/private/views.py
+++ portal-backend/depmap/private/views.py
@@ -16,19 +16,17 @@ blueprint = Blueprint(
 )
 
 
-@blueprint.route("/<bucket_name>/<filename>")
+@blueprint.route("/<bucket_name>/<path:filename>")
 def portal_file_private_url(bucket_name, filename):
     private_file_buckets = current_app.config.get("PRIVATE_FILE_BUCKETS")
     # Make sure bucket provided is accepted in valid list of buckets
     if private_file_buckets is None or bucket_name not in private_file_buckets:
         abort(404)
 
-    try:
-        credentials = ServiceAccountCredentials.from_json_keyfile_name(
-            current_app.config["DOWNLOADS_KEY"]
-        )
-        signed_url = sign_url(credentials, bucket_name, filename)
-    except Exception as e:
-        raise e
+    credentials = ServiceAccountCredentials.from_json_keyfile_name(
+        current_app.config["DOWNLOADS_KEY"]
+    )
+    signed_url = sign_url(credentials, bucket_name, filename)
+
     # Redirect to generated signed GCS url
     return redirect(signed_url)
