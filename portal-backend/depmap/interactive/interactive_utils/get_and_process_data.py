69f59ade2cb1d37dde52e5bff6774cf6a1aab922
Sarah Wessel <swessel@broadinstitute.org>
2024-09-03 10:44:54 -0400

    test get_dataset_sample_labels_by_id

--- portal-backend/depmap/interactive/interactive_utils/get_and_process_data.py
+++ portal-backend/depmap/interactive/interactive_utils/get_and_process_data.py
@@ -148,12 +148,11 @@ def get_dataset_sample_labels_by_id(dataset_id) -> dict[str, str]:
     Samples from the legacy backend are always depmap_models, so it's safe to
     hard-code that labels should always be cell line display names. 
     """
-    # TODO: write a unit test for this
     dataset_sample_ids = get_dataset_sample_ids(dataset_id)
     all_model_labels_by_id = CellLine.get_cell_line_display_name_series().to_dict()
     return {
         id: label
-        for label, id in all_model_labels_by_id.items()
+        for id, label in all_model_labels_by_id.items()
         if id in dataset_sample_ids
     }
 
