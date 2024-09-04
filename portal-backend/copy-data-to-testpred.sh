be3b111f978f39c02a38bd7de8bda947ad00f3a3
Philip Montgomery <pmontgom@broadinstitute.org>
2024-09-04 15:08:40 -0400

    added statement to delete correlations in copy-data-to-testpred.sh

--- portal-backend/copy-data-to-testpred.sh
+++ portal-backend/copy-data-to-testpred.sh
@@ -5,5 +5,5 @@ DEPMAP_ENV=testpred
 set -ex
 
 rsync --progress --exclude results --exclude '*private*' --exclude '*-cor*sqlite3' -avc webapp_data/ "ubuntu@dev.cds.team:/data2/depmap/${DEPMAP_ENV}/data/"
-
+ssh ubuntu@dev.cds.team "echo \"delete from correlated_dataset;\" | sqlite3 /data2/depmap/${DEPMAP_ENV}/data/data.db"
 
