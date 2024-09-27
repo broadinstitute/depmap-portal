af416525f368487a1998688609159a8791e7cca2
Sarah Wessel <swessel@broadinstitute.org>
2024-09-27 09:42:58 -0400

    Test compound queries

--- portal-backend/tests/depmap/data_explorer_2/views/test_evaluate_v2_context.py
+++ portal-backend/tests/depmap/data_explorer_2/views/test_evaluate_v2_context.py
@@ -157,7 +157,99 @@ def test_evaluate_v2_context_slice_queries(app, empty_db_mock_downloads):
 
         assert response["ids"] == ["2", "3"]
 
-        # TODO: write more examples for this
+    # Test: Get models which have a dependency of < .5 on gene1
+    basic_context_request = {
+        "context": {
+            "dimension_type": "depmap_model",
+            "name": "dependency greater than",
+            "expr": {"<": [{"var": "gene_var"}, 0.5]},
+            "vars": {
+                "gene_var": {
+                    "dataset_id": dataset_id,
+                    "identifier": "gene1",
+                    "identifier_type": "feature_label",
+                }
+            },
+        },
+    }
+
+    with app.test_client() as c:
+        # Get the single model value belonging to gene0
+        r = c.post(
+            url_for("data_explorer_2.evaluate_v2_context"),
+            data=json.dumps(basic_context_request),
+            content_type="application/json",
+        )
+        assert r.status_code == 200, r.status_code
+        # get dimension returns a compressed response, which needs to be unzipped
+        response = json.loads(gzip.decompress(r.data).decode("utf8"))
+
+        assert response["ids"] == ["ACH-1", "ACH-2"]
 
 
-# TODO: write test that uses multiple datasets (and combines multiple queries)
+def test_evaluate_v2_context_compound_expressions(app, empty_db_mock_downloads):
+    """
+    Test the evaluation of v2 contexts with multiple slice query expressions. 
+    """
+    gene0 = GeneFactory(label="gene0")
+    gene1 = GeneFactory(label="gene1")
+    gene2 = GeneFactory(label="gene2")
+    cell_lines = CellLineFactory.create_batch(3)
+    dataset_name = DependencyEnum.Chronos_Combined
+    dataset_id = dataset_name.name
+    crispr_dataset = DependencyDatasetFactory(
+        matrix=MatrixFactory(
+            entities=[gene0, gene1, gene2],
+            cell_lines=cell_lines,
+            data=[
+                [1.0, 0.1, 0.01],  # gene0, entrez_id 1
+                [2.0, 0.2, 0.02],  # gene1, entrez_id 2
+                [3.0, 0.3, 0.03],  # gene2, entrez_id 3
+            ],
+        ),
+        name=dataset_name,
+        priority=1,
+    )
+
+    empty_db_mock_downloads.session.flush()
+    interactive_test_utils.reload_interactive_config()
+
+    # Test: Get genes which have BOTH:
+    # - a dependency > .15 in the model ACH-1
+    # - and a dependency < 0.025 in model ACH-2
+    basic_context_request = {
+        "context": {
+            "dimension_type": "gene",
+            "name": "dependency greater than",
+            "expr": {
+                "and": [
+                    {">": [{"var": "model1_var"}, 0.15]},
+                    {"<": [{"var": "model2_var"}, 0.025]},
+                ]
+            },
+            "vars": {
+                "model1_var": {
+                    "dataset_id": dataset_id,
+                    "identifier": "ACH-1",
+                    "identifier_type": "sample_id",
+                },
+                "model2_var": {
+                    "dataset_id": dataset_id,
+                    "identifier": "ACH-2",
+                    "identifier_type": "sample_id",
+                },
+            },
+        },
+    }
+    with app.test_client() as c:
+        # Get the single model value belonging to gene0
+        r = c.post(
+            url_for("data_explorer_2.evaluate_v2_context"),
+            data=json.dumps(basic_context_request),
+            content_type="application/json",
+        )
+        assert r.status_code == 200, r.status_code
+        # get dimension returns a compressed response, which needs to be unzipped
+        response = json.loads(gzip.decompress(r.data).decode("utf8"))
+
+        assert response["ids"] == ["2"]
