a0d9e3b9c582c3fce1bb0092f4892abd501a2bea
Jessica Cheng <44929570+jessica-cheng@users.noreply.github.com>
2024-10-23 09:31:19 -0400

    Resources page new accordions (#97)
    * Replace buggy Accordion component with React bootstrap Panel
    
    * Create types for Subcategory and Topic
    
    * Make Panel for subcategory into controlled component to keep track of toggle state
    
    * Remove log

---
+++ frontend/packages/portal-frontend/src/resources/components/SubcategoryPanel.tsx
@@ -0,0 +1,78 @@
+import * as React from "react";
+import { useState } from "react";
+import { ListGroup, ListGroupItem, Panel } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import styles from "src/resources/styles/ResourcesPage.scss";
+import { Subcategory, Topic } from "../models/Category";
+
+/**
+ * Make Panel into a controlled component so that we can keep track of each Panel's toggle state and render the correct glyphicon symbol for open/close
+ */
+
+interface SubcategoryPanelProps {
+  subcategory: Subcategory;
+  isDefaultExpandedSubcategory: boolean;
+  selectedTopic?: Topic | null;
+}
+
+export default function SubcategoryPanel({
+  subcategory,
+  isDefaultExpandedSubcategory,
+  selectedTopic = undefined,
+}: SubcategoryPanelProps) {
+  const [isOpen, setIsOpen] = useState(isDefaultExpandedSubcategory);
+
+  return (
+    <Panel
+      key={subcategory.id}
+      eventKey={subcategory.slug}
+      defaultExpanded={isDefaultExpandedSubcategory}
+      onToggle={(e) => {
+        setIsOpen(e);
+      }}
+    >
+      <Panel.Heading className={styles.postHeading}>
+        <Panel.Toggle componentClass="div">
+          {subcategory.title}
+          <span
+            className={
+              isOpen ? "glyphicon glyphicon-minus" : "glyphicon glyphicon-plus"
+            }
+            aria-hidden="true"
+            style={{
+              float: "right",
+              margin: "0",
+              position: "relative",
+              top: "50%",
+              lineHeight: "unset",
+            }}
+          />
+        </Panel.Toggle>
+      </Panel.Heading>
+      <Panel.Collapse>
+        <ListGroup>
+          {subcategory.topics.map((topic: Topic) => {
+            return (
+              <Link
+                key={topic.id}
+                to={`?subcategory=${subcategory.slug}&topic=${topic.slug}`}
+                state={{ postHtml: topic }}
+                style={{ textDecoration: "none" }}
+              >
+                <ListGroupItem
+                  className={styles.navPostItem}
+                  style={{ borderRadius: "0px" }}
+                  active={
+                    selectedTopic ? selectedTopic.slug === topic.slug : false
+                  }
+                >
+                  {topic.title}
+                </ListGroupItem>
+              </Link>
+            );
+          })}
+        </ListGroup>
+      </Panel.Collapse>
+    </Panel>
+  );
+}
