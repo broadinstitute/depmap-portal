5a5d7b41400e1f3a6ce1e6a873d690beecd90f82
Sarah Wessel <swessel@broadinstitute.org>
2024-11-01 09:51:29 -0400

    Generate client before docker build

--- breadbox/commands.py
+++ breadbox/commands.py
@@ -230,7 +230,7 @@ def update_client():
             "poetry",
             "run",
             "-C",
-            "breadbox-client-generator",
+            "breadbox-client-generator/breadbox_client_generator",
             "openapi-python-client",
             "generate",
             "--meta=none",
