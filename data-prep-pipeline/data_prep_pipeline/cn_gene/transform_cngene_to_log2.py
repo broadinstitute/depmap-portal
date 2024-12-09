7e408365b7d9b378b2c8e8c8374111562ec51614
Nayeem Aquib <101162634+naquib314@users.noreply.github.com>
2024-12-09 08:10:17 -0500

    CN Gene Transformation using conseq

--- data-prep-pipeline/data_prep_pipeline/cn_gene/transform_cngene_to_log2.py
+++ data-prep-pipeline/data_prep_pipeline/cn_gene/transform_cngene_to_log2.py
@@ -1,34 +1,57 @@
+import argparse
 import numpy as np
-import pandas as pd
 from taigapy import create_taiga_client_v3
 
-from datarelease_taiga_permanames import (
-    cngene_taiga_permaname,
-    cngene_log2_taiga_permaname,
-)
+import sys
+
+sys.path.append(".")
 from utils import update_taiga
 
 
-def transform_cngene_to_log2(df: pd.DataFrame):
-    """Transform data to log2 scale."""
-    return np.log2(df + 1)
-
-
-def process_and_update_cngene_log2(source_dataset_id, target_dataset_id):
-    """Transform CN gene expression data to log2 scale and upload to Taiga."""
-
+def transform_cngene_to_log2(cngene_dataset_id):
+    """Transform CN gene expression data to log2 scale"""
     tc = create_taiga_client_v3()
 
     print("Getting CN gene expression data...")
-    cngene_expression_data = tc.get(f"{source_dataset_id}/{cngene_taiga_permaname}")
+    cngene_expression_data = tc.get(cngene_dataset_id)
 
     print("Transforming CN gene expression data to log2 scale...")
-    log2_transformed_data = transform_cngene_to_log2(cngene_expression_data)
+    log2_transformed_data = np.log2(cngene_expression_data + 1)
     print("Transformed CN gene expression data to log2 scale")
 
-    update_taiga(
-        log2_transformed_data,
-        "Transform CN gene expression data to log2 scale",
-        target_dataset_id,
-        cngene_log2_taiga_permaname,
+    return log2_transformed_data
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        description="Transform CN gene expression data to log2 scale."
     )
+    parser.add_argument(
+        "cngene_dataset_id", help="Dataset ID of CN gene expression data"
+    )
+    parser.add_argument("output", help="Path to write the output")
+    parser.add_argument(
+        "-u",
+        "--update",
+        nargs="?",
+        const=True,
+        default=False,
+        help="Update the dataset with the transformed data",
+    )
+    args = parser.parse_args()
+    log2_transformed_data = transform_cngene_to_log2(args.cngene_dataset_id)
+    log2_transformed_data.to_csv(args.output)
+
+    if args.update:
+        dataset_id = (
+            args.update
+            if isinstance(args.update, str)
+            else args.cngene_dataset_id.split(".")[0]
+        )
+        update_taiga(
+            log2_transformed_data,
+            "Transform CN gene expression data to log2 scale",
+            dataset_id,
+            "PortalOmicsCNGeneLog2",
+        )
+        print("Updated dataset with transformed data")
