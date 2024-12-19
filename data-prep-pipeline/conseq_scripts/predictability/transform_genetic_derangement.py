93cdc66dc3a3c34b226bf2738db51223aca9bb96
Nayeem Aquib <101162634+naquib314@users.noreply.github.com>
2024-12-19 14:18:54 -0500

    Added some predictability transformations

--- data-prep-pipeline/conseq_scripts/predictability/transform_genetic_derangement.py
+++ data-prep-pipeline/conseq_scripts/predictability/transform_genetic_derangement.py
@@ -1,21 +1,17 @@
-from taigapy import create_taiga_client_v3
-from utils import update_taiga
-from datarelease_taiga_permanames import cngene_log2_taiga_permaname
-from config import hgnc_gene_table_taiga_id
+import argparse
+import pandas as pd
 
 
-def process_and_update_genetic_derangement(source_dataset_id, target_dataset_id):
+def process_and_generate_genetic_derangement(cngene_log2_csv, hgnc_gene_table_csv):
 
-    """Generate genetic derangement data for predictability and upload it to Taiga."""
-
-    tc = create_taiga_client_v3()
+    """Generate genetic derangement data for predictability"""
 
     print("Getting log2 transformed CNGene data...")
-    cn_data = tc.get(f"{source_dataset_id}/{cngene_log2_taiga_permaname}")
+    cn_data = pd.read_csv(cngene_log2_csv)
     cn_stripped = cn_data.copy()
     cn_stripped.columns = [s.split(" ")[0] for s in cn_data.columns]
 
-    hgnc_gene_table = tc.get(f"{hgnc_gene_table_taiga_id}")
+    hgnc_gene_table = pd.read_csv(hgnc_gene_table_csv)
     cytoband_data = hgnc_gene_table[["symbol", "location"]]
 
     print("Generating genetic derangement data...")
@@ -24,9 +20,20 @@ def process_and_update_genetic_derangement(source_dataset_id, target_dataset_id)
     ).mean()
     print("Generated genetic derangement data")
 
-    update_taiga(
-        genetic_derangement_matrix,
-        "Generate genetic derangement data for predictability",
-        target_dataset_id,
-        "PredictabilityGenticDerangementTransformed",
+    return genetic_derangement_matrix
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        description="Generate Gentetic Derangement matrix for predictability"
     )
+    parser.add_argument("cngene_log2_csv", help="Path to log2 transformed CNGene data")
+    parser.add_argument("hgnc_gene_table_csv", help="Path to HGNC gene table")
+    parser.add_argument("output", help="Path to write the output")
+    args = parser.parse_args()
+    genetic_derangement_matrix = process_and_generate_genetic_derangement(
+        args.cngene_log2_csv, args.hgnc_gene_table_csv
+    )
+
+    if genetic_derangement_matrix is not None:
+        genetic_derangement_matrix.to_csv(args.output, index=False)
