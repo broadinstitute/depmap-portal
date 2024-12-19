33ec9a3bd993afa8687703b2969287ebb2498cf8
Philip Montgomery <pmontgom@broadinstitute.org>
2024-12-18 21:18:27 -0500

    added flag to download_csv_and_view_interactive for de2 or de1

--- portal-backend/depmap/interactive/views.py
+++ portal-backend/depmap/interactive/views.py
@@ -864,6 +864,7 @@ def download_csv_and_view_interactive():
     display_name = request.args["display_name"]
     units = request.args["units"]
     file_url = request.args["url"]
+    use_de2 = request.args.get("de2", "T") == "T"
 
     url_upload_whitelist = flask.current_app.config["URL_UPLOAD_WHITELIST"]
 
@@ -882,7 +883,7 @@ def download_csv_and_view_interactive():
         abort(400)
 
     result = upload_transient_csv.apply(
-        args=[display_name, units, True, csv_path, True]
+        args=[display_name, units, True, csv_path, False, use_de2]
     )
 
     if result.state == TaskState.SUCCESS.value:
