0d246c7f6ff5a67aeaf5af6a443db460e05f3670
Jessica Cheng <jcheng@broadinstitute.org>
2025-01-27 17:00:19 -0500

    Change expected properties in ErrorDetail to reflect error response object

--- frontend/packages/@depmap/types/src/ErrorDetail.ts
+++ frontend/packages/@depmap/types/src/ErrorDetail.ts
@@ -1,14 +1,7 @@
-interface BodyDetailError {
-  body: { detail: string };
-}
-
-interface DetailError {
+interface ErrorDetail {
   detail: string;
 }
 
-// TODO: Verify whether this backwards compatibility safeguard is necessary. Error response verified to follow DetailError type definition so far
-type ErrorDetail = BodyDetailError | DetailError;
-
 export function instanceOfErrorDetail(object: any): object is ErrorDetail {
-  return "detail" in object || ("body" in object && "detail" in object.body);
+  return "detail" in object;
 }
