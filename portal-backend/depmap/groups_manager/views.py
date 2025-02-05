bbb442f509d1057a6ee0178783eff7bbb733dd47
Jessica Cheng <jcheng@broadinstitute.org>
2025-02-05 17:22:26 -0500

    Add feature flag to not show dataset and group managers in public portal

--- portal-backend/depmap/groups_manager/views.py
+++ portal-backend/depmap/groups_manager/views.py
@@ -1,8 +1,5 @@
 import logging
-from flask import (
-    Blueprint,
-    render_template,
-)
+from flask import Blueprint, render_template, current_app, abort
 
 
 log = logging.getLogger(__name__)
@@ -17,4 +14,6 @@ def view_groups_manager():
     """
     Entry point for groups manager
     """
+    if current_app.config["ENABLED_FEATURES"].is_public():
+        abort(404)
     return render_template("groups_manager/index.html")
