c2296d3019c958e9174ef4797d2b2abc50f6c135
Philip Montgomery <pmontgom@broadinstitute.org>
2025-02-08 07:49:53 -0500

    WIP

---
+++ pipeline/scripts/cor_table_packer.py
@@ -0,0 +1,202 @@
+import struct
+import zlib
+import sqlite3
+import pandas as pd
+import numpy as np
+
+from dataclasses import dataclass
+
+
+@dataclass
+class InputMatrixDesc:
+    given_ids: list[str]
+    taiga_id: str
+    name: str
+
+
+ROW_BYTE_SIZE = 4 + 4 + 4  # 32 bit int, 32 bit float, 32 bit float
+
+
+def write_dim_labels(cursor, dim_i, given_ids):
+    cursor.execute(
+        f"create table dim_{dim_i}_given_id (dim_{dim_i} integer, given_id varchar)"
+    )
+    cursor.executemany(
+        f"insert into dim_{dim_i}_given_id (dim_{dim_i} , given_id ) values (?, ?)",
+        list(enumerate(given_ids)),
+    )
+
+
+def write_cor_df(
+    df: pd.DataFrame,
+    dim_0_desc: InputMatrixDesc,
+    dim_1_desc: InputMatrixDesc,
+    filename: str,
+):
+    """Writes a sqlite3 table which packs the correlations for a given feature into a single compressed blob"""
+    assert set(df.columns) == set(["dim_0", "dim_1", "cor", "log10qvalue"])
+
+    rows = []
+
+    for dim_0, group in df.groupby("dim_0"):
+        # sort indices to reduce the amount of entropy in the column and compress better
+        # the other columns are floats, so they'll compress poorly regardless
+        sorted_group = group.copy().sort_values("dim_1")
+
+        buf = bytearray()
+        buf.extend(sorted_group["dim_1"].values.astype("int32").tobytes())
+        buf.extend(sorted_group["cor"].values.astype("float32").tobytes())
+        buf.extend(sorted_group["log10qvalue"].values.astype("float32").tobytes())
+
+        cbuf = zlib.compress(buf)
+        rows.append((dim_0, cbuf))
+
+    # create file and write out everything
+    conn = sqlite3.connect(filename)
+    conn.execute("create table correlation (dim_0 int primary key, cbuf blob)")
+    conn.executemany("insert into correlation(dim_0, cbuf) values (?, ?)", rows)
+
+    write_dim_labels(conn, 0, dim_0_desc.given_ids)
+    write_dim_labels(conn, 1, dim_1_desc.given_ids)
+
+    pd.DataFrame(
+        dict(
+            dim_index=[0, 1],
+            taiga_id=[dim_0_desc.taiga_id, dim_1_desc.taiga_id],
+            dataset_given_id=[dim_0_desc.name, dim_1_desc.name],
+        )
+    ).to_sql("dataset", conn, if_exists="replace", index=False)
+
+    print(f"Building indices...")
+    for dim_i in [0, 1]:
+        conn.execute(
+            f"CREATE INDEX dim_{dim_i}_given_id_idx_1 ON dim_{dim_i}_given_id (given_id)"
+        )
+        conn.execute(
+            f"CREATE INDEX dim_{dim_i}_given_id_idx_2 ON dim_{dim_i}_given_id (dim_{dim_i})"
+        )
+
+    conn.commit()
+    conn.close()
+
+
+def read_cor_for_given_id(filename, feature_id):
+    conn = sqlite3.connect(filename)
+
+    # fetch the blob for the given feature
+    cursor = conn.cursor()
+    cursor.execute(
+        "select c.dim_0, c.cbuf from correlation c join dim_0_given_id f on f.dim_0=c.dim_0 where f.given_id = ?",
+        [feature_id],
+    )
+    row = cursor.fetchone()
+    if row is None:
+        return pd.DataFrame(
+            {
+                "dim_0": [],
+                "dim_1": [],
+                "cor": [],
+                "log10qvalue": [],
+                "feature_given_id_0": [],
+                "dataset_given_id_0": [],
+                "feature_given_id_1": [],
+                "dataset_given_id_1": [],
+            }
+        )
+    index, cbuf = row
+    # now unpack the value
+    buf = zlib.decompress(cbuf)
+    row_count = len(buf) // ROW_BYTE_SIZE
+    start = 0
+    end = 4 * row_count
+    dim_1 = np.frombuffer(buf[start:end], dtype="int32")
+    start = end
+    end += 4 * row_count
+    cor = np.frombuffer(buf[start:end], dtype="float32")
+    start = end
+    end += 4 * row_count
+    log10qvalue = np.frombuffer(buf[start:end], dtype="float32")
+
+    df = pd.DataFrame(
+        {"dim_0": index, "dim_1": dim_1, "cor": cor, "log10qvalue": log10qvalue}
+    )
+
+    def map_dim_index_to_given_ids(dim_i, positions):
+        indices = list(set(positions))
+        param_str = ",".join(["?"] * len(indices))
+        cursor.execute(
+            f"select given_id, dim_{dim_i} from dim_{dim_i}_given_id where dim_{dim_i} in ({ param_str })",
+            indices,
+        )
+        position_to_label = {i: given_id for given_id, i in cursor.fetchall()}
+        return [position_to_label[position] for position in positions]
+
+    cursor.execute("select dim_index, dataset_given_id from dataset")
+    given_id_by_dataset_index = {
+        dim_index: given_id for dim_index, given_id in cursor.fetchall()
+    }
+
+    df["feature_given_id_0"] = map_dim_index_to_given_ids(0, df["dim_0"])
+    df["feature_given_id_1"] = map_dim_index_to_given_ids(1, df["dim_1"])
+    df["dataset_given_id_0"] = given_id_by_dataset_index[0]
+    df["dataset_given_id_1"] = given_id_by_dataset_index[1]
+
+    cursor.close()
+    conn.close()
+
+    return df.drop(columns=["dim_0", "dim_1"])
+
+
+def test_write_df(tmpdir):
+    df = pd.DataFrame(
+        {
+            "dim_0": [0, 0, 1, 1],
+            "dim_1": [1, 2, 3, 0],
+            "cor": [0.1, 0.2, 0.001, 0.9],
+            "log10qvalue": [1e-10, 1e-1, 0.1, 1e-12],
+        }
+    )
+    out = str(tmpdir.join("out.sqlite3"))
+    write_cor_df(
+        df,
+        InputMatrixDesc(
+            given_ids=["C1", "C2", "C3", "C4"], taiga_id="crispr-taiga", name="crispr"
+        ),
+        InputMatrixDesc(
+            given_ids=["E1", "E2", "E3", "E4"], taiga_id="expr-taiga", name="expr"
+        ),
+        out,
+    )
+
+    df = read_cor_for_given_id(out, "C1").sort_values("feature_given_id_1")
+
+    def round_list(l):
+        return [float(f"{x:.4}") for x in l]
+
+    assert list(df["feature_given_id_1"]) == ["E2", "E3"]
+    assert round_list(df["cor"]) == [0.1, 0.2]
+
+    df = read_cor_for_given_id(out, "C2").sort_values("feature_given_id_1")
+    assert list(df["feature_given_id_1"]) == ["E1", "E4"]
+    assert round_list(df["cor"]) == [0.9, 0.001]
+
+
+# at the end of the day, we want this to return a df with columns:
+# (compound dataset) taiga_id, (compound feature) given_id, compound_name, compound_dose, dataset_name, measurement,
+# cor_taiga_id, cor_given_id, cor_label, cor_dataset_name, cor, qvalue
+
+# in the UI transform that into
+# interface CompoundCorrelation {
+#   measurement: string;
+#   dose? : number;
+#   featureDataset : string;
+#   featureLabel : string;
+#   cor :number;
+#   negLog10QValue: number;
+# }
+
+
+# Add filters for:
+#   Dose + Measurement
+#   correlated_dataset_name
+#   correlated_label
---
+++ pipeline/scripts/correlation_with_qvalue.py
@@ -0,0 +1,410 @@
+import argparse
+import sqlite3
+import sys
+import numpy as np
+import pandas as pd
+import taigapy
+from taigapy.client_v3 import LocalFormat
+from tqdm import tqdm
+from dataclasses import dataclass
+
+
+@dataclass
+class Thresholds:
+    batch_size: int
+    limit: int
+    minsamples: int
+    limit_per_sign: int
+    max_qvalue: float
+
+
+sys.path.append(".")
+from cor_table_packer import write_cor_df, InputMatrixDesc, read_cor_for_given_id
+import json
+
+
+def read_parameters(filename):
+    tc = taigapy.create_taiga_client_v3()
+
+    with open(filename, "rt") as fd:
+        parameters = json.load(fd)
+
+    a_mat = tc.get(parameters["a_taiga_id"])
+    b_mat = tc.get(parameters["b_taiga_id"])
+
+    return (
+        a_mat,
+        parameters["a_given_id"],
+        parameters["a_taiga_id"],
+        b_mat,
+        parameters["b_given_id"],
+        parameters["b_taiga_id"],
+    )
+
+
+def compute_cor_table(
+    in_hdf5_0_df,
+    label_0,
+    taiga_id_0,
+    in_hdf5_1_df,
+    label_1,
+    taiga_id_1,
+    output_file,
+    thresholds: Thresholds,
+):
+    in_hdf5_0_df, in_hdf5_1_df = with_shared_cell_lines(in_hdf5_0_df, in_hdf5_1_df)
+    # if we don't have a lot of samples shared, something is wrong
+    assert len(in_hdf5_0_df) > thresholds.minsamples
+    assert len(in_hdf5_1_df) == len(in_hdf5_0_df)
+    correlations_df = create_correlations_df(in_hdf5_0_df, in_hdf5_1_df, thresholds)
+    correlations_df["log10qvalue"] = np.log10(correlations_df["qvalue"])
+    correlations_df = correlations_df[["dim_0", "dim_1", "cor", "log10qvalue"]]
+
+    print(f"Writing to {output_file}")
+    write_cor_df(
+        correlations_df,
+        InputMatrixDesc(
+            given_ids=list(in_hdf5_0_df.columns), taiga_id=taiga_id_0, name=label_0
+        ),
+        InputMatrixDesc(
+            given_ids=list(in_hdf5_1_df.columns), taiga_id=taiga_id_1, name=label_1
+        ),
+        output_file,
+    )
+    print("Done")
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("input_json")
+    parser.add_argument("--minsamples", type=int, default=30)
+    parser.add_argument("--batchsize", type=int, default=500)
+    parser.add_argument("--limit", type=int, default=250)
+    parser.add_argument("--maxqvalue", type=float, default=0.1)
+    parser.add_argument("--limit-per-sign", type=int, default=25, dest="limit_per_sign")
+    parser.add_argument("output_file")
+
+    args = parser.parse_args()
+
+    (
+        in_hdf5_0_df,
+        label_0,
+        taiga_id_0,
+        in_hdf5_1_df,
+        label_1,
+        taiga_id_1,
+    ) = read_parameters(args.input_json)
+
+    compute_cor_table(
+        in_hdf5_0_df,
+        label_0,
+        taiga_id_0,
+        in_hdf5_1_df,
+        label_1,
+        taiga_id_1,
+        args.output_file,
+        Thresholds(
+            args.batchsize,
+            args.limit,
+            args.minsamples,
+            args.limit_per_sign,
+            args.maxqvalue,
+        ),
+    )
+
+
+def with_shared_cell_lines(dep_df, biomarker_df):
+    shared_cell_lines = np.intersect1d(dep_df.index, biomarker_df.index)
+    dep_df = dep_df.loc[shared_cell_lines]
+    biomarker_df = biomarker_df.loc[shared_cell_lines]
+    return dep_df, biomarker_df
+
+
+from scipy import stats
+
+
+def _calc_cor_pq_values(n, c):
+    # n is the number of pairs of values used to compute the correlation
+    # c is the pearson correlation for which we want the p-value
+
+    dist = stats.beta(n / 2 - 1, n / 2 - 1, loc=-1, scale=2)
+    p = 2 * dist.cdf(-abs(c))
+
+    if np.isfinite(p).all():
+        q = stats.false_discovery_control(p, axis=1)
+    else:
+        q = np.array(p)
+        q[np.isfinite(p)] = stats.false_discovery_control(p[np.isfinite(p)])
+    return p, np.asarray(q)
+
+
+def chunk(values, chunksize):
+    for i in range(0, len(values), chunksize):
+        yield values[i : i + chunksize]
+
+
+def create_correlation_dfs(dep_df, biomarker_df, batchsize, min_samples):
+    biomarker_df = biomarker_df.copy()
+    # assumes rows have already been aligned
+    biomarker_df.columns = list(range(len(biomarker_df.columns)))
+    dep_df_indices = np.arange(len(dep_df.columns))
+
+    for dep_df_partial_indices in tqdm(list(chunk(dep_df_indices, batchsize))):
+
+        dep_df_partial = dep_df.iloc[:, dep_df_partial_indices]
+
+        correlations, sample_counts = fast_cor_with_missing(
+            dep_df_partial.values, biomarker_df.values, min_samples
+        )
+        p, q = _calc_cor_pq_values(sample_counts, correlations)
+
+        for partial_i, dep_col_i in enumerate(dep_df_partial_indices):
+            yield dep_col_i, pd.DataFrame(
+                {
+                    "dim_1": np.arange(correlations.shape[1]),
+                    "cor": correlations[partial_i],
+                    "samples": sample_counts[partial_i],
+                    "pvalue": p[partial_i],
+                    "qvalue": q[partial_i],
+                }
+            )
+
+
+def filter_df(df, limit, limit_per_sign, max_qvalue):
+    # Taken from PRISM portal: Only the top 250 features, or top 25 negative and positive
+    # correlations based on q-value at each condition are included in the plots. Associations
+    # with q-values above 0.1 are omitted from both plot and table.
+    cor_col = df["cor"]
+    top_250_abs = (-cor_col.abs()).rank() <= limit
+    top_25_neg = cor_col.mask(cor_col >= 0, np.nan).rank() <= limit_per_sign
+    top_25_pos = cor_col.mask(cor_col <= 0, np.nan).rank() <= limit_per_sign
+    small_q = df["qvalue"] < max_qvalue
+    return df[(top_250_abs | top_25_neg | top_25_pos) & small_q]
+
+
+def create_correlations_df(m_0, m_1, thresholds: Thresholds):
+    dfs = []
+    for m_0_col, cor_df in create_correlation_dfs(
+        m_0, m_1, thresholds.batch_size, thresholds.minsamples
+    ):
+        cor_df = filter_df(
+            cor_df, thresholds.limit, thresholds.limit_per_sign, thresholds.max_qvalue
+        ).copy()
+        cor_df["dim_0"] = m_0_col
+        dfs.append(cor_df)
+    return pd.concat(dfs)
+
+
+# def create_correlations_df_partial(dep_df, biomarker_df_partial, limit, min_samples, limit_per_sign):
+
+#     (
+#         abs_top_ranked_cols_per_row,
+#         abs_top_ranked_rows_per_col,
+#         abs_row_indexes,
+#         abs_col_indexes,
+#     ) = top_ranked_indexes_per_row_and_col(-np.abs(correlations), limit)
+
+#     (
+#         neg_top_ranked_cols_per_row,
+#         neg_top_ranked_rows_per_col,
+#         neg_row_indexes,
+#         neg_col_indexes,
+#     ) = top_ranked_indexes_per_row_and_col(_new_masked_matrix(correlations, correlations>=0), limit_per_sign)
+
+#     (
+#         pos_top_ranked_cols_per_row,
+#         pos_top_ranked_rows_per_col,
+#         pos_row_indexes,
+#         pos_col_indexes,
+#     ) = top_ranked_indexes_per_row_and_col(_new_masked_matrix(correlations, correlations<=0), limit_per_sign)
+
+#     dfs = []
+
+#     for (top_ranked_cols_per_row,
+#         top_ranked_rows_per_col,
+#         row_indexes,
+#         col_indexes) in [
+#             (
+# abs_top_ranked_cols_per_row,
+#         abs_top_ranked_rows_per_col,
+#         abs_row_indexes,
+#         abs_col_indexes),
+#         (neg_top_ranked_cols_per_row,
+#         neg_top_ranked_rows_per_col,
+#         neg_row_indexes,
+#         neg_col_indexes),
+
+# (
+#         pos_top_ranked_cols_per_row,
+#         pos_top_ranked_rows_per_col,
+#         pos_row_indexes,
+#         pos_col_indexes,
+#     )
+#         ]:
+
+#         df = pd.DataFrame(
+#         {
+#             "cor": np.hstack(
+#                 (
+#                     correlations[row_indexes, top_ranked_cols_per_row],
+#                     correlations[top_ranked_rows_per_col, col_indexes],
+#                 )
+#             ),
+#             "samples": np.hstack(
+#                 (
+#                     sample_counts[row_indexes, top_ranked_cols_per_row],
+#                     sample_counts[top_ranked_rows_per_col, col_indexes],
+#                 ))
+#             ,
+#             "dim_0": list(row_indexes) + list(top_ranked_rows_per_col),
+#             "dim_1": biomarker_df_partial.columns[
+#                 list(top_ranked_cols_per_row) + list(col_indexes)
+#             ],
+#         },
+#         columns=["dim_0", "dim_1", "cor", "samples"],
+#         )
+#         dfs.append(df)
+
+#     merged_df = pd.concat(dfs)
+#     return merged_df.drop_duplicates(["dim_0", "dim_1"])
+
+
+def top_ranked_indexes_per_row_and_col(matrix, limit):
+    """Gets the coordinates for the largest `LIMIT` values, by row and by column."""
+    num_rows, num_cols = matrix.shape
+    limit_per_col = min(num_rows, limit)
+    limit_per_row = min(num_cols, limit)
+
+    top_ranked_cols_per_row = np.argpartition(matrix, limit_per_row - 1, axis=1)[
+        :, :limit_per_row
+    ].flatten()
+    top_ranked_rows_per_col = np.argpartition(matrix, limit_per_col - 1, axis=0)[
+        :limit_per_col
+    ].flatten()
+
+    row_indexes = np.repeat(range(num_rows), limit_per_row)
+    col_indexes = np.tile(range(num_cols), limit_per_col)
+
+    return top_ranked_cols_per_row, top_ranked_rows_per_col, row_indexes, col_indexes
+
+
+def fast_cor_with_missing(x, y, min_number_of_points):
+    # preallocate storage for the matrix of correlations
+    result = np.zeros(shape=(x.shape[1], y.shape[1]))
+    # preallocate storage for the matrix of n (number of pairwise-non-NA samples)
+    sample_count = np.zeros(shape=(x.shape[1], y.shape[1]), dtype=int)
+
+    x_groups = group_cols_with_same_mask(x)
+    y_groups = group_cols_with_same_mask(y)
+    for x_mask, x_columns in x_groups:
+        for y_mask, y_columns in y_groups:
+            # print(x_mask, x_columns, y_mask, y_columns)
+            combined_mask = x_mask & y_mask
+            number_of_points = np.sum(combined_mask)
+            if number_of_points < min_number_of_points:
+                # skip computing the correlation for these pairs, but just record that we've got a
+                # hole by storing NA in the result matrix
+                result[np.ix_(x_columns, y_columns)] = np.nan
+            else:
+                # not sure if this is the fastest way to slice out the relevant subset
+                x_without_holes = x[:, x_columns][combined_mask, :]
+                y_without_holes = y[:, y_columns][combined_mask, :]
+
+                c = np_pearson_cor(x_without_holes, y_without_holes)
+                # update result with these correlations
+                result[np.ix_(x_columns, y_columns)] = c
+            sample_count[np.ix_(x_columns, y_columns)] = number_of_points
+
+    return result, sample_count
+
+
+def group_cols_with_same_mask(x):
+    """
+    Group columns with the same indexes of NAN values.
+    
+    Return a sequence of tuples (mask, columns) where columns are the column indices
+    in x which all have the mask.
+    """
+    per_mask = {}
+    for i in range(x.shape[1]):
+        o_mask = np.isfinite(x[:, i])
+        o_mask_b = np.packbits(o_mask).tobytes()
+        if o_mask_b not in per_mask:
+            per_mask[o_mask_b] = [o_mask, []]
+        per_mask[o_mask_b][1].append(i)
+    return per_mask.values()
+
+
+def np_pearson_cor(x, y):
+    """Full column-wise Pearson correlations of two matrices."""
+    xv = x - x.mean(axis=0)
+    yv = y - y.mean(axis=0)
+    xvss = (xv * xv).sum(axis=0)
+    yvss = (yv * yv).sum(axis=0)
+    # print(xvss, yvss)
+    # print(np.matmul(xv.transpose(), yv) , np.sqrt(np.outer(xvss, yvss)))
+    result = np.matmul(xv.transpose(), yv) / np.sqrt(np.outer(xvss, yvss))
+    return np.maximum(np.minimum(result, 1.0), -1.0)
+
+
+def concat_dfs_and_filter(dfs, limit):
+    # todo: update to respect signed limit
+    df = pd.concat(dfs, ignore_index=True, sort=False)
+    df["cor_abs"] = df["cor"].abs()
+    df["dim_0_rank"] = df.groupby("dim_1")["cor_abs"].rank(ascending=False)
+    df["dim_1_rank"] = df.groupby("dim_0")["cor_abs"].rank(ascending=False)
+    df = df[(df["dim_0_rank"] <= limit) | (df["dim_1_rank"] <= limit)]
+    del df["cor_abs"]
+    del df["dim_0_rank"]
+    del df["dim_1_rank"]
+    return df
+
+
+def test_small_end_to_end(tmpdir):
+    output_file = str(tmpdir.join("out.sqlite3"))
+
+    in_hdf5_0_df = pd.DataFrame(
+        [[1, -1], [2, 1], [3, -1], [4, 1]],
+        columns=["A", "B"],
+        index=["ACH-01", "ACH-02", "ACH-03", "ACH-04"],
+    )
+    label_0 = "X"
+    taiga_id_0 = "taiga-x"
+    in_hdf5_1_df = pd.DataFrame(
+        [[10, -10], [20, 10], [30, 10], [40, -10]],
+        columns=["C", "D"],
+        index=["ACH-01", "ACH-02", "ACH-03", "ACH-04"],
+    )
+    label_1 = "Y"
+    taiga_id_1 = "taiga-y"
+    compute_cor_table(
+        in_hdf5_0_df,
+        label_0,
+        taiga_id_0,
+        in_hdf5_1_df,
+        label_1,
+        taiga_id_1,
+        output_file,
+        Thresholds(
+            batch_size=100, limit=100, minsamples=0, limit_per_sign=100, max_qvalue=100
+        ),
+    )
+
+    df = read_cor_for_given_id(output_file, "A")
+
+    rows = df.sort_values("feature_given_id_1").to_dict("records")
+    assert rows[0]["dataset_given_id_0"] == "X"
+    assert rows[0]["dataset_given_id_1"] == "Y"
+    assert rows[0]["feature_given_id_0"] == "A"
+    assert rows[0]["feature_given_id_1"] == "C"
+    assert abs(rows[0]["cor"] - 1.0) < 1e-5
+
+    assert rows[1]["dataset_given_id_0"] == "X"
+    assert rows[1]["dataset_given_id_1"] == "Y"
+    assert rows[1]["feature_given_id_0"] == "A"
+    assert rows[1]["feature_given_id_1"] == "D"
+    assert abs(rows[1]["cor"] - 0.0) < 1e-5
+
+
+if __name__ == "__main__":
+    main()
