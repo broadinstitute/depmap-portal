b96f0c8bfcb9a58afdf9972a2825fd95be46178b
Philip Montgomery <pmontgom@broadinstitute.org>
2025-03-12 15:42:50 -0400

    disable private datasets for envs except DMC
    We're shutting them down entirely on march 31st and this
    is causing problems with test envs

--- portal-backend/depmap/settings/settings.py
+++ portal-backend/depmap/settings/settings.py
@@ -163,7 +163,7 @@ class FeatureFlags:
 
     @property
     def private_datasets(self):
-        return self.is_prerelease_env()
+        return self.is_only_dmc_and_i_have_a_good_reason()
 
     @property
     def target_discovery_app(self):
