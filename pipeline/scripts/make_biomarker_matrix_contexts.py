db3e40691c9d9782415f864f2fc92f30182da91a
Ali Mourey <90707472+alimourey@users.noreply.github.com>
2025-03-26 17:21:06 -0400

    Fix preprocessing of contexts

--- pipeline/scripts/make_biomarker_matrix_contexts.py
+++ pipeline/scripts/make_biomarker_matrix_contexts.py
@@ -2,19 +2,21 @@ import argparse
 from pipeline.scripts.hdf5_utils import write_hdf5
 
 
-def main(subtype_context_taiga_id):
+def main(subtype_context_taiga_id, out_hdf5_filename, out_filename):
     from taigapy import create_taiga_client_v3
 
     tc = create_taiga_client_v3()
     one_hot_encoded_context_matrix = tc.get(subtype_context_taiga_id)
     bool_matrix = one_hot_encoded_context_matrix.astype(bool)
     bool_matrix.set_index("cell_line", inplace=True)
-    write_hdf5(bool_matrix.transpose(), "temp-out.hdf5")
-    bool_matrix.to_csv("out.csv")
+    write_hdf5(bool_matrix.transpose(), out_hdf5_filename)
+    bool_matrix.to_csv(out_filename)
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("subtype_context_taiga_id")
+    parser.add_argument("out_hdf5_filename")
+    parser.add_argument("out_filename")
     args = parser.parse_args()
-    main(args.subtype_context_taiga_id)
+    main(args.subtype_context_taiga_id, args.out_hdf5_filename, args.out_filename)
