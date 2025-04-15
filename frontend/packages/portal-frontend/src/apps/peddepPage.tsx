cb6c6cab8be74992b6ebcb9746809a1247fba1cc
Jessica Cheng <jcheng@broadinstitute.org>
2025-04-15 16:25:47 -0400

    Create peddep page component; Add text and styling for text

--- frontend/packages/portal-frontend/src/apps/peddepPage.tsx
+++ frontend/packages/portal-frontend/src/apps/peddepPage.tsx
@@ -2,6 +2,7 @@ import "src/public-path";
 import React from "react";
 import ReactDOM from "react-dom";
 import ErrorBoundary from "src/common/components/ErrorBoundary";
+import PeddepPage from "src/peddepLandingPage/components/PeddepPage";
 
 const container = document.getElementById("peddep-page");
 const dataElement = document.getElementById("peddep-page-data");
@@ -17,10 +18,7 @@ if (!dataElement || !dataElement.textContent) {
 const App = () => {
   return (
     <ErrorBoundary>
-      <div>
-        The answer to life, the universe, and everything is:
-        {/* {secretOfTheUniverse} */}
-      </div>
+      <PeddepPage />
     </ErrorBoundary>
   );
 };
---
+++ frontend/packages/portal-frontend/src/peddepLandingPage/components/PeddepPage.tsx
@@ -0,0 +1,124 @@
+import * as React from "react";
+import { Button } from "react-bootstrap";
+import { getDapi } from "src/common/utilities/context";
+import styles from "src/peddepLandingPage/styles/PeddepPage.scss";
+
+interface PeddepPageProps {}
+
+export default function ResourcesPage(props: PeddepPageProps) {
+  const imagePath = getDapi()._getFileUrl(
+    // TODO: Use context?
+    "/static/img/public/RainbowMap_2x.png"
+  );
+
+  return (
+    <div>
+      <div className={styles.PeddepPage}>
+        <div className={styles.highlight}>
+          <div className={styles.highlightText}>
+            <h1 className={styles.title}>
+              The Pediatric Cancer Dependencies Accelerator
+            </h1>
+            <h3>
+              St. Jude Children&apos;s Research Hospital, The Broad Institute,
+              and Dana-Farber Cancer Institute have launched a large-scale
+              collaboration to advance understanding of the biological basis of
+              pediatric cancers, identify new vulnerabilities of these diseases
+              and accelerate therapies globally.
+            </h3>
+            <Button>Learn more at PedDep.org</Button>
+          </div>
+        </div>
+      </div>
+      <hr />
+      <div className={styles.PeddepPage}>
+        <h2>Our Goals and Focus</h2>
+        <h4>
+          The PedDep Accelerator is leading a multi-pronged effort against this
+          problem focusing both on expanding and extending known successful
+          approaches as well as investing in exploratory science with
+          transformative potential.
+        </h4>
+        <div className={styles.dataContainer}>
+          <div className={styles.dataInfo}>
+            <div>
+              <h4>Dependency Screening</h4>
+              <h5>
+                Developing and deploying CRISPR-based genome editing techniques
+                to identify hidden vulnerabilities (dependencies) in a spectrum
+                of high-risk childhood brain, solid and hematological
+                malignancies.
+              </h5>
+            </div>
+            <div>
+              <h4>Omics profiling</h4>
+              <h5>
+                Leveraging emerging technologies to characterize the genetic and
+                epigenetic landscape of pediatric cancers.
+              </h5>
+            </div>
+            <div>
+              <h4>Compound screening</h4>
+              <h5>
+                Developing and deploying CRISPR-based genome editing techniques
+                to identify hidden vulnerabilities (dependencies) in a spectrum
+                of high-risk childhood brain, solid and hematological
+                malignancies.
+              </h5>
+            </div>
+            <div>
+              <h4>New Model Derivation</h4>
+              <h5>
+                Developing model systems where none currently exist for
+                high-risk childhood cancers that have poor outcomes.
+              </h5>
+            </div>
+            <div>
+              <h4>Data science</h4>
+              <h5>
+                Developing computational approaches to mine and integrate data
+                and developing innovative software tools for data sharing.
+              </h5>
+            </div>
+          </div>
+          <div>
+            <p>INPut graph here</p>
+          </div>
+        </div>
+      </div>
+      <hr />
+      <div className={styles.PeddepPage}>
+        <h2>A Pediatric Context</h2>
+        <h4>
+          Navigate the portal with a pediatric context. We&apos;ve built this
+          context to include models that represent pediatric tumor types.
+        </h4>
+      </div>
+      <hr />
+      <div className={styles.PeddepPage}>
+        <div className={styles.aboutPeddep}>
+          <div>
+            <h2>The First Generation Pediatric Dependency Map</h2>
+            <h4>
+              The PedDep initiative started in 2014 with the goal of applying
+              the Broad&apos;s DepMap large-scale genetic dependencies and drug
+              sensitivity approaches to accelerate the discovery of therapeutic
+              targets and strategies for pediatric patients.
+            </h4>
+            <h4>
+              While clinical trials per se are not a component of the PedDep
+              project, generating data which would allow the start of clinical
+              trials is within the purview. PedDep was developed to generate the
+              core data, break down silos, and use the combination of expertises
+              to synergize ability to conduct groundbreaking research.
+            </h4>
+            <h4>
+              Read our landmark paper in Nature Genetics or visit our PedDep
+              Explorer.
+            </h4>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
