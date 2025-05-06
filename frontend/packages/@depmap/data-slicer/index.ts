779baeee1a0d7ae08e486e32ce152714566f4d28
Ali Coppolino <90707472+alimourey@users.noreply.github.com>
2025-05-06 16:37:28 -0400

    Fix current release data page subtypes (#257)

--- frontend/packages/@depmap/data-slicer/index.ts
+++ frontend/packages/@depmap/data-slicer/index.ts
@@ -1,4 +1,4 @@
-export { FileSubType, ReleaseType, ViewMode } from "./src/models/downloads";
+export { FileSubtype, ReleaseType, ViewMode } from "./src/models/downloads";
 export { ValidationTextbox } from "./src/components/ValidationTextbox";
 export { default as DatasetPicker } from "./src/components/DatasetPicker";
 export { default as DownloadTracker } from "./src/components/DownloadTracker";
