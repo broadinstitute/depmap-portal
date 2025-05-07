ac6b16747ed461a279c1bae046e1f815c6cf1f6b
Philip Montgomery <pmontgom@broadinstitute.org>
2025-05-07 16:38:09 -0400

    Switched the creation of the fusion matrix to use canonical fusion names

--- pipeline/scripts/make_fusions_matrix.py
+++ pipeline/scripts/make_fusions_matrix.py
@@ -25,35 +25,8 @@ def main():
     tc = create_taiga_client_v3()
     fusions = tc.get(args.fusions_dataset_id)
 
-    # use the hgnc dataset to normalize gene symbols
-    sym_map = tc.get(args.hgnc_dataset_id)
-
-    # goal: normalize symbols by looking up ensemble IDs to use our latest symbol set and construct a one-hot encoded matrix
-    # with DepMap cell line IDs as row labels and columns are of the form "LEFTGENE_RIGHTGENE" where "LEFTGENE" and
-    # "RIGHTGENE" are represented by gene symbols.
-
-    symbol_by_ensembl = {
-        rec["ensembl_gene_id"]: rec["symbol"] for rec in sym_map.to_records()
-    }
-
-    def make_fusion_name(left, right):
-        left_ensembl = extract_id(left)
-        right_ensembl = extract_id(right)
-        left_symbol = symbol_by_ensembl.get(left_ensembl)
-        right_symbol = symbol_by_ensembl.get(right_ensembl)
-        if (
-            left_ensembl is None
-            or right_ensembl is None
-            or left_symbol is None
-            or right_symbol is None
-        ):
-            # fall back to the symbols in the orignal left and right gene names if we can't figure it out
-            left_symbol = left.split(" ")[0]
-            right_symbol = right.split(" ")[0]
-        return f"{left_symbol}_{right_symbol}"
-
     fusions["fusion_name"] = [
-        make_fusion_name(rec["Gene1"], rec["Gene2"]) for rec in fusions.to_records()
+        rec["CanonicalFusionName"] for rec in fusions.to_records()
     ]
 
     fusions["one"] = 1
