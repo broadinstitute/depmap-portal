64e3cea65ee85a95e18e0b6499a3be3d70186ca0
Ali Mourey <90707472+alimourey@users.noreply.github.com>
2025-05-15 10:03:03 -0400

    Fix typo in ModelConditionID

--- pipeline/celligner/scripts/run_celligner.py
+++ pipeline/celligner/scripts/run_celligner.py
@@ -284,7 +284,7 @@ def process_depmap_ipts(expr_df, context_df, prof_map, model_condition):
             "subtype",
             "type",
             "FormulationID",
-            "ModelCondition",
+            "ModelConditionID",
             "ModelID",
         ],
     ]
