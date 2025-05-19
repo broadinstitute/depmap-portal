5f6450baa774547993394e0aa2c01ed5edf33151
Philip Montgomery <pmontgom@broadinstitute.org>
2025-05-19 13:44:09 -0400

    moved user settings from /temp to /user

--- breadbox/breadbox/api/temp/__init__.py
+++ breadbox/breadbox/api/temp/__init__.py
@@ -2,7 +2,6 @@ from .router import router
 
 # import the following which register endpoints onto `router` as a side effect of being imported
 import breadbox.api.temp.cas
-import breadbox.api.temp.user_settings
 import breadbox.api.temp.context
 import breadbox.api.temp.associations
 
