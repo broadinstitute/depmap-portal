0a9f7427d1f5a532da2729af504b28975e070c10
Nayeem Aquib <101162634+naquib314@users.noreply.github.com>
2025-05-28 11:13:55 -0400

    Using Mapped for data_type

--- breadbox/breadbox/models/data_type.py
+++ breadbox/breadbox/models/data_type.py
@@ -2,6 +2,7 @@ from sqlalchemy import (
     Column,
     String,
 )
+from sqlalchemy.orm import Mapped
 
 from breadbox.db.base_class import Base
 
@@ -9,4 +10,4 @@ from breadbox.db.base_class import Base
 class DataType(Base):
     __tablename__ = "data_type"
 
-    data_type = Column(String, primary_key=True)
+    data_type: Mapped[str] = Column(String, primary_key=True)
