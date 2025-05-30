3d4f1786dcb087b61f50ea418d8d44d58f3ca8bd
Nayeem Aquib <101162634+naquib314@users.noreply.github.com>
2025-05-30 04:47:07 -0400

    Get the mapper using inspect for bulk_update_mappings

--- breadbox/breadbox/crud/dimension_types.py
+++ breadbox/breadbox/crud/dimension_types.py
@@ -1,7 +1,7 @@
 from typing import Dict, List, Literal, Optional, Type, Union
 from uuid import uuid4
 
-from sqlalchemy import and_
+from sqlalchemy import and_, inspect
 
 import pandas as pd
 import numpy as np
@@ -237,7 +237,9 @@ def update_dataset_dimensions_with_dimension_type(
     if dimension_type.axis == "feature":  # TODO: add label field to DatasetSample too..
         for i in range(0, len(updated_dimension_labels), 10000):  # arbitrary chunk size
             chunk = i + 10000
-            db.bulk_update_mappings(DatasetFeature, updated_dimension_labels[i:chunk])
+            db.bulk_update_mappings(
+                inspect(DatasetFeature), updated_dimension_labels[i:chunk]
+            )
     db.flush()
 
 
