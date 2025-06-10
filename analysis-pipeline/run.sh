b833bf1a85c1b5956f1e52e5c33d40f622fe83d6
Philip Montgomery <pmontgom@broadinstitute.org>
2025-06-10 03:04:44 +0000

    Corrections to the docker file for the pipeline runner Also includes publishing

---
+++ analysis-pipeline/run.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/bash
+
+set -ex
+
+# all files that the pipeline uses must be accessible under the current working directory
+# in order for the docker container to access it. So, copy things that are from outside of this tree
+# before starting. Maybe this should be moved into run_pipeline.py ?
+#mkdir -p extern
+#cp ../pipeline/preprocess_taiga_ids.py extern
+
+GOOGLE_APPLICATION_CREDENTIALS=$HOME/.secrets/depmap-pipeline-runner.json exec ./run_pipeline.py \
+  --publish-dest gs://preprocessing-pipeline-outputs/depmap-pipeline/test-pred/metadata --image test --env internal "$@"
+
