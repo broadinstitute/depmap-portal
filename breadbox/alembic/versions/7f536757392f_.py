14f958ed5a912fe1b4e71184805a59645cf5362d
Philip Montgomery <pmontgom@broadinstitute.org>
2025-06-11 22:28:28 -0400

    merge two alembic heads

---
+++ breadbox/alembic/versions/7f536757392f_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: 7f536757392f
+Revises: 020788c82611, 1bd39c06e7e3
+Create Date: 2025-06-11 22:27:52.582173
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = "7f536757392f"
+down_revision = ("020788c82611", "1bd39c06e7e3")
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
