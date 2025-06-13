c4d1d79bd5b4984db9e884167459f917392b3c2d
Jessica Cheng <jcheng@broadinstitute.org>
2025-06-13 14:50:43 -0400

    Create stacked bar chart

--- frontend/packages/portal-frontend/src/peddepLandingPage/components/GroupedBarSuplots.tsx
+++ frontend/packages/portal-frontend/src/peddepLandingPage/components/GroupedBarSuplots.tsx
@@ -1,37 +1,75 @@
 import React, { useEffect, useRef } from "react";
 import Plotly, { Config, Layout, PlotData } from "plotly.js";
-import { BarSubplotData } from "../models/subplotData";
+import { BarSubplotData, Subgroup } from "../models/subplotData";
 
 interface GroupedBarSuplotsProp {
-  subplotsData: BarSubplotData[];
+  subplotsData: any;
+  countData: Record<string, number>;
 }
 
 export default function GroupedBarSuplots(props: GroupedBarSuplotsProp) {
-  const { subplotsData } = props;
+  const { subplotsData, countData } = props;
+  console.log(subplotsData);
 
   const plotRef = useRef(null);
 
   useEffect(() => {
     if (plotRef.current) {
-      const plotlyData: Partial<PlotData>[] = subplotsData.map(
-        (subplotData) => {
-          return {
-            x: subplotData.xAxisLabels,
-            y: subplotData.values,
-            text: subplotData.labels,
-            name: subplotData.name,
+      // Define subgroup colors
+      const subgroupToColor: {
+        [key in Subgroup]: { color: string; line: string };
+      } = {
+        "CNS/Brain": { color: "#f5db84", line: "#f5c422" },
+        Heme: { color: "#d3b2db", line: "#ba37db" },
+        Solid: { color: "#a3bce6", line: "#1154bf" },
+      };
+      const subtypes = Array.from(new Set(subplotsData.map((d) => d.subtype)));
+      const subgroups = Array.from(
+        new Set(subplotsData.map((d) => d.subgroup))
+      );
+      const subfeatures = Array.from(
+        new Set(subplotsData.map((d) => d.subtypeFeature))
+      );
+      console.log(subtypes);
+      console.log(subgroups);
+      console.log(subfeatures);
+
+      const traces: Partial<Plotly.PlotData>[] = [];
+
+      subgroups.forEach((subgroup) => {
+        subfeatures.forEach((subfeature, i) => {
+          traces.push({
+            x: subtypes,
+            y: subtypes.map((subtype) => {
+              return countData[`${subgroup}-${subtype}-${subfeature}`] ?? 0;
+            }),
+            text: subtypes.map((subtype) => {
+              const count =
+                countData[`${subgroup}-${subtype}-${subfeature}`] ?? 0;
+              return count > 0
+                ? `Parent: ${subgroup}<br>Type: ${subtype}<br>Subtype: ${subfeature}<br>Count: ${count}`
+                : "";
+            }),
+            hoverinfo: "text",
+            name: i === 0 ? subgroup : "", // Only first subtype gets legend label
+            showlegend: i === 0,
+            legendgroup: subgroup,
+            offsetgroup: subgroup,
+            stackgroup: subgroup,
             type: "bar",
             marker: {
-              color: subplotData.color,
+              color: subgroupToColor[subgroup as Subgroup].color,
               line: {
-                color: subplotData.lineColor,
+                color: subgroupToColor[subgroup as Subgroup].line,
                 width: 2,
               },
             },
-            hoverinfo: "y+text",
-          };
-        }
-      );
+          });
+        });
+      });
+      console.log("Traces: ", traces);
+
+      console.log(traces);
 
       const layout: Partial<Layout> = {
         title: "",
@@ -46,7 +84,7 @@ export default function GroupedBarSuplots(props: GroupedBarSuplotsProp) {
           //   zeroline: false,
           gridwidth: 1,
         },
-        barmode: "group",
+        barmode: "stack",
         legend: {
           orientation: "h",
           xanchor: "right",
@@ -61,6 +99,7 @@ export default function GroupedBarSuplots(props: GroupedBarSuplotsProp) {
           t: 50,
           pad: 4,
         },
+        hovermode: "closest",
       };
 
       const config: Partial<Config> = {
@@ -70,9 +109,9 @@ export default function GroupedBarSuplots(props: GroupedBarSuplotsProp) {
         displayModeBar: false,
       };
 
-      Plotly.react(plotRef.current, plotlyData, layout, config);
+      Plotly.react(plotRef.current, traces, layout, config);
     }
-  }, [subplotsData]);
+  }, [countData, subplotsData]);
 
   return (
     <div>
