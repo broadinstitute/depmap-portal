4cb063f59c45716f90a1cb292edacbc11549205d
Ali Mourey <90707472+alimourey@users.noreply.github.com>
2025-07-02 21:43:00 -0400

    Take out hack we don't need anymore

--- frontend/packages/portal-frontend/src/compound/doseCurvesTab/DoseCurvesPlotSection.tsx
+++ frontend/packages/portal-frontend/src/compound/doseCurvesTab/DoseCurvesPlotSection.tsx
@@ -53,17 +53,8 @@ function DoseCurvesPlotSection({
     [curvesData]
   );
 
-  const [doseCurveKey, setDoseCurveKey] = React.useState(0);
-
-  React.useEffect(() => {
-    const handler = () => setDoseCurveKey((k) => k + 1);
-    window.addEventListener("changeTab:dose-curves-new", handler);
-    return () =>
-      window.removeEventListener("changeTab:dose-curves-new", handler);
-  }, []);
-
   return (
-    <div className={styles.PlotSection} key={doseCurveKey}>
+    <div className={styles.PlotSection}>
       <div className={styles.sectionHeader}>
         {plotElement && (
           <PlotControls
