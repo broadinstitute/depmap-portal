799a53cb33a00517b659df7ad3224024305ccfa4
Randy Creasi <rcreasi@broadinstitute.org>
2025-07-28 18:23:09 -0400

    fix test

--- frontend/packages/@depmap/data-explorer-2/src/components/DimensionSelect/__tests__/DimensionSelect.test.tsx
+++ frontend/packages/@depmap/data-explorer-2/src/components/DimensionSelect/__tests__/DimensionSelect.test.tsx
@@ -1,14 +1,25 @@
 import "@testing-library/jest-dom";
 import { render, screen, waitFor } from "@testing-library/react";
 import * as React from "react";
+import { dataExplorerAPI } from "../../../services/dataExplorerAPI";
+import { deprecatedDataExplorerAPI } from "../../../services/deprecatedDataExplorerAPI";
 import DimensionSelect from "../index";
 import { NULL_MAPPING } from "../useDimensionStateManager/useData";
 
-// TODO: mock these methods:
-// fetchDatasetsByIndexType: {() => Promise.resolve({ depmap_model: [] })
-// fetchDimensionLabelsToDatasetsMapping: () => { return Promise.resolve(NULL_MAPPING); }
-
 test("hides the aggregation select for the special case of correlation", async () => {
+  dataExplorerAPI.fetchDatasetsByIndexType = jest
+    .fn<ReturnType<typeof dataExplorerAPI.fetchDatasetsByIndexType>, []>()
+    .mockResolvedValue({ depmap_model: [] });
+
+  deprecatedDataExplorerAPI.fetchDimensionLabelsToDatasetsMapping = jest
+    .fn<
+      ReturnType<
+        typeof deprecatedDataExplorerAPI.fetchDimensionLabelsToDatasetsMapping
+      >,
+      []
+    >()
+    .mockResolvedValue(NULL_MAPPING);
+
   render(
     <DimensionSelect
       mode="entity-or-context"
