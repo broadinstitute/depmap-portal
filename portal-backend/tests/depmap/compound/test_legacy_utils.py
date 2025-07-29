e409dea037ce5db82a55288a1d02a58e761b6375
Ali Mourey <90707472+alimourey@users.noreply.github.com>
2025-07-29 16:40:45 -0400

    Add test

--- portal-backend/tests/depmap/compound/test_legacy_utils.py
+++ portal-backend/tests/depmap/compound/test_legacy_utils.py
@@ -1,5 +1,5 @@
-from typing import Optional
 from depmap.compound.legacy_utils import (
+    does_legacy_dataset_exist_with_compound_experiment,
     get_compound_ids_by_experiment_id,
     get_compound_labels_by_experiment_label,
 )
@@ -14,10 +14,7 @@ from tests.factories import (
 from tests.utilities import interactive_test_utils
 
 
-def _setup_factories(
-    empty_db_mock_downloads,
-    dataset_name: Optional[DependencyDataset.DependencyEnum] = None,
-):
+def test_get_compound_ids_by_experiment_id(empty_db_mock_downloads):
     # Compound 1 has 3 corresponding experiments (2 of which are in our dataset)
     compound1 = CompoundFactory()
     compound_experiment_1A = CompoundExperimentFactory(compound=compound1)
@@ -35,32 +32,10 @@ def _setup_factories(
         [DepmapModelFactory(), DepmapModelFactory(), DepmapModelFactory()],
         using_depmap_model_table=True,
     )
-    dataset = (
-        DependencyDatasetFactory(matrix=matrix)
-        if not dataset_name
-        else DependencyDatasetFactory(matrix=matrix, name=dataset_name)
-    )
+    dataset = DependencyDatasetFactory(matrix=matrix)
     dataset_id = dataset.name.name
     empty_db_mock_downloads.session.flush()
     interactive_test_utils.reload_interactive_config()
-
-    return (
-        dataset_id,
-        compound_experiment_1A,
-        compound_experiment_2,
-        compound1,
-        compound2,
-    )
-
-
-def test_get_compound_ids_by_experiment_id(empty_db_mock_downloads):
-    (
-        dataset_id,
-        compound_experiment_1A,
-        compound_experiment_2,
-        compound1,
-        compound2,
-    ) = _setup_factories(empty_db_mock_downloads)
 
     result = get_compound_ids_by_experiment_id(dataset_id)
     assert list(result.keys()) == [
@@ -77,13 +52,26 @@ def test_get_compound_ids_by_experiment_id(empty_db_mock_downloads):
 
 def test_get_compound_labels_by_experiment_labels(empty_db_mock_downloads):
     # Compound 1 has 3 corresponding experiments (2 of which are in our dataset)
-    (
-        dataset_id,
-        compound_experiment_1A,
-        compound_experiment_2,
-        compound1,
-        compound2,
-    ) = _setup_factories(empty_db_mock_downloads)
+    compound1 = CompoundFactory()
+    compound_experiment_1A = CompoundExperimentFactory(compound=compound1)
+    compound_experiment_1B = CompoundExperimentFactory(compound=compound1)
+    compound_experiment_1C = CompoundExperimentFactory(compound=compound1)
+    # Compound 2 only has one experiment (which is in our dataset)
+    compound2 = CompoundFactory()
+    compound_experiment_2 = CompoundExperimentFactory(compound=compound2)
+    # Compound 3 has one experiment (which is not in our dataset)
+    compound3 = CompoundFactory()
+    compound_experiment_3 = CompoundExperimentFactory(compound=compound3)
+    # multiple cell lines so can plot distplot
+    matrix = MatrixFactory(
+        [compound_experiment_1A, compound_experiment_1B, compound_experiment_2,],
+        [DepmapModelFactory(), DepmapModelFactory(), DepmapModelFactory()],
+        using_depmap_model_table=True,
+    )
+    dataset = DependencyDatasetFactory(matrix=matrix)
+    dataset_id = dataset.name.name
+    empty_db_mock_downloads.session.flush()
+    interactive_test_utils.reload_interactive_config()
 
     result = get_compound_labels_by_experiment_label(dataset_id)
     assert list(result.keys()) == [
@@ -95,34 +83,72 @@ def test_get_compound_labels_by_experiment_labels(empty_db_mock_downloads):
 
 
 def test_does_legacy_dataset_exist_with_compound_experiment(empty_db_mock_downloads):
-    # Returns False if:
-    (
-        dataset_id,
-        compound_experiment_1A,
-        compound_experiment_2,
-        compound1,
-        compound2,
-    ) = _setup_factories(empty_db_mock_downloads)
+    dataset_name = DependencyDataset.DependencyEnum.Prism_oncology_AUC
+    dataset_name_repurposing = DependencyDataset.DependencyEnum.Rep_all_single_pt
 
-    # 1. dataset does not exist (setup does not define an Prism_oncology_AUC or Rep_all_single_pt dataset)
-    dataset = DependencyDataset.get_dataset_by_name(
-        DependencyDataset.DependencyEnum.Prism_oncology_AUC.value
+    # Compound 1 has 3 corresponding experiments (2 of which are in our dataset)
+    compound1 = CompoundFactory()
+    compound_experiment_1A = CompoundExperimentFactory(compound=compound1)
+    compound_experiment_1B = CompoundExperimentFactory(compound=compound1)
+    # Compound 2 only has one experiment (which is in our dataset)
+    compound2 = CompoundFactory()
+    compound_experiment_2 = CompoundExperimentFactory(compound=compound2)
+    # Compound 3 has one experiment (which is not in our dataset)
+    compound3 = CompoundFactory()
+    compound_experiment_3 = CompoundExperimentFactory(compound=compound3)
+
+    matrix = MatrixFactory(
+        [compound_experiment_1A, compound_experiment_1B, compound_experiment_2,],
+        [
+            DepmapModelFactory(depmap_id="a"),
+            DepmapModelFactory(depmap_id="b"),
+            DepmapModelFactory(depmap_id="c"),
+        ],
+        using_depmap_model_table=True,
     )
-    assert dataset is None
-
-    # 2. compound_exp_dataset is not found with matching dataset
-
-    #
-
-
-def test_dataset_exist(empty_db_mock_downloads):
-    (
-        dataset_id,
-        compound_experiment_1A,
-        compound_experiment_2,
-        compound1,
-        compound2,
-    ) = _setup_factories(
-        empty_db_mock_downloads,
-        dataset_name=DependencyDataset.DependencyEnum.Prism_oncology_AUC,
+    oncref_dataset = DependencyDatasetFactory(matrix=matrix, name=dataset_name)
+    rep_dataset = DependencyDatasetFactory(
+        matrix=MatrixFactory(), name=dataset_name_repurposing
     )
+
+    empty_db_mock_downloads.session.flush()
+    interactive_test_utils.reload_interactive_config()
+
+    compound_exp_and_dataset = [
+        (compound_experiment_2, rep_dataset),
+        (compound_experiment_1A, oncref_dataset),
+    ]
+
+    # 1. Return True if the dataset exists with that compound experiment
+    dataset_exists_w_compound = does_legacy_dataset_exist_with_compound_experiment(
+        dataset_name=dataset_name.value,
+        compound_exp_and_dataset=compound_exp_and_dataset,
+    )
+    assert dataset_exists_w_compound is True
+
+    # 2. The dataset exists and the compound experiment exist, but the compound experiment is not in the dataset of interest.
+    dataset_exists_w_compound = does_legacy_dataset_exist_with_compound_experiment(
+        dataset_name=dataset_name_repurposing.value,
+        compound_exp_and_dataset=compound_exp_and_dataset,
+    )
+    assert dataset_exists_w_compound is False
+
+    dataset_no_ce = [
+        (compound_experiment_3, rep_dataset),
+        (compound_experiment_3, oncref_dataset),
+    ]
+    # 3. The dataset exists, but the compound we are searching for is not in that dataset.
+    dataset_exists_w_compound = does_legacy_dataset_exist_with_compound_experiment(
+        dataset_name=dataset_name.value, compound_exp_and_dataset=dataset_no_ce
+    )
+    assert dataset_exists_w_compound is False
+
+    ce_no_dataset = [
+        (compound_experiment_3, DependencyDataset()),
+        (compound_experiment_3, DependencyDataset()),
+    ]
+    # 4. The dataset exists, but the compound we are searching for is not in that dataset.
+    dataset_exists_w_compound = does_legacy_dataset_exist_with_compound_experiment(
+        dataset_name=dataset_name.value, compound_exp_and_dataset=ce_no_dataset
+    )
+    assert dataset_exists_w_compound is False
