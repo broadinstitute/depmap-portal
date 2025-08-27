dfc42ac81101b284cbba02e6e7e1e4a487f9159d
Ali Mourey <90707472+alimourey@users.noreply.github.com>
2025-08-27 17:32:36 -0400

    Add Load Gene Context section

---
+++ frontend/packages/portal-frontend/src/geneTea/components/LoadFromGeneContextSection.tsx
@@ -0,0 +1,124 @@
+import React, { useState } from "react";
+import {
+  ContextSelector,
+  deprecatedDataExplorerAPI,
+  getDimensionTypeLabel,
+  pluralize,
+} from "@depmap/data-explorer-2";
+import { DepMap } from "@depmap/globals";
+import { DataExplorerContext } from "@depmap/types";
+import { useGeneTeaContext } from "../context/GeneTeaContext";
+import { useEffect } from "react";
+
+const LoadFromGeneContextSection: React.FC = () => {
+  const {
+    handleSetGeneSymbolSelections,
+    handleSetValidGeneSymbols,
+    handleSetInValidGeneSymbols,
+    allAvailableGenes,
+    geneSymbolSelections,
+  } = useGeneTeaContext();
+
+  useEffect(() => {
+    if (geneSymbolSelections.size === 0 && value !== null) {
+      setValue(null);
+    }
+  }, [geneSymbolSelections]);
+
+  const indexType = "gene";
+
+  const [value, setValue] = useState<DataExplorerContext | null>(null);
+
+  const [stats, setStats] = useState<{
+    total: number;
+    notFound: number;
+    hiddenByFilters: number;
+  } | null>(null);
+
+  const numberedEntities = (n: number) => {
+    const entity = getDimensionTypeLabel(indexType);
+    const entities = pluralize(entity);
+
+    return [n.toLocaleString(), n === 1 ? entity : entities].join(" ");
+  };
+
+  const handleChange = async (nextContext: DataExplorerContext | null) => {
+    // If the context is changing, the gene list sent to GeneTEA is changing. Clear out the Valid
+    // and Invalid gene sets in preparation to receive a new valid/invalid list from the GeneTEA api.
+    handleSetValidGeneSymbols(new Set([]));
+    handleSetInValidGeneSymbols(new Set([]));
+    setValue(nextContext);
+
+    if (!nextContext) {
+      setStats(null);
+      handleSetGeneSymbolSelections([]);
+
+      return;
+    }
+
+    const labels = await deprecatedDataExplorerAPI.evaluateLegacyContext(
+      nextContext
+    );
+    const contextLabels = new Set(labels);
+
+    handleSetGeneSymbolSelections(contextLabels);
+
+    const found = [...allAvailableGenes].filter((label) => {
+      return contextLabels.has(label);
+    }).length;
+
+    const notFound = contextLabels.size - found;
+
+    setStats({
+      total: contextLabels.size,
+      notFound,
+      hiddenByFilters: 0,
+    });
+
+    const selectionLength = contextLabels.size - notFound;
+
+    if (selectionLength === 0) {
+      console.log("OK");
+    } else {
+      console.log(`Select ${numberedEntities(selectionLength)}`);
+    }
+  };
+
+  return (
+    <div>
+      <ContextSelector
+        show
+        enable
+        value={value}
+        onChange={handleChange}
+        onClickCreateContext={() => {
+          DepMap.saveNewContext(
+            { context_type: indexType },
+            null,
+            handleChange
+          );
+        }}
+        context_type={indexType}
+        onClickSaveAsContext={() => {}}
+        includeAllInOptions={false}
+      />
+      {stats && (
+        <div style={{ marginTop: 15 }}>
+          <div>
+            This context contains a total of {numberedEntities(stats.total)}.
+          </div>
+          <br />
+          {Boolean(stats.notFound) && (
+            <div>
+              {numberedEntities(stats.notFound)}{" "}
+              {stats.notFound === 1 ? "is" : "are"} not found in the selected
+              dataset(s).
+            </div>
+          )}
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default LoadFromGeneContextSection;
--- frontend/packages/portal-frontend/src/geneTea/components/SearchOptionsContainer.tsx
+++ frontend/packages/portal-frontend/src/geneTea/components/SearchOptionsContainer.tsx
@@ -1,23 +1,30 @@
-import React, { useContext } from "react";
+import React from "react";
 import styles from "../styles/GeneTea.scss";
 import { Tab, Tabs } from "react-bootstrap";
-import MultiSelectTextarea from "./MultiSelectTextExperimental";
 import SectionStack, {
-  SectionStackContext,
   StackableSection,
 } from "./collapsibleOptions/SectionStack";
 import PlotOptionsPanel from "./PlotOptionsPanel";
 import TermOptionsPanel from "./TermOptionsPanel";
 import MultiSelectTextArea from "./MultiSelectTextArea";
+import LoadFromGeneContextSection from "./LoadFromGeneContextSection";
 
 function SearchOptionsContainer() {
-  const { sectionHeights } = useContext(SectionStackContext);
-
   return (
     <div className={styles.SearchOptionsContainer}>
       <Tabs className={styles.Tabs} id={"gene-tea-filter-tabs"}>
         <Tab eventKey={"List"} title={"List"} className={styles.Tab}>
           <MultiSelectTextArea />
+          <div
+            style={{
+              marginLeft: "25px",
+              marginRight: "25px",
+              marginBottom: "25px",
+            }}
+          >
+            <h4 className={styles.sectionTitle}>Load Gene Context</h4>
+            <LoadFromGeneContextSection />
+          </div>
           <SectionStack>
             <StackableSection title="Plot Options" minHeight={132}>
               <PlotOptionsPanel />
