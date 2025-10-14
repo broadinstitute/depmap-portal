f2029e54962ab2eb143c8bc5be7c37091c87e1d7
Randy Creasi <rcreasi@broadinstitute.org>
2025-10-14 11:57:53 -0400

    add support for Breadbox version of MSI matrix

--- frontend/packages/@depmap/data-explorer-2/src/constants/wellKnownDatasets.ts
+++ frontend/packages/@depmap/data-explorer-2/src/constants/wellKnownDatasets.ts
@@ -1,8 +1,10 @@
+const legacy_msi = "legacy_msi";
 const mutations_prioritized = "prioritized-mutations-test";
 const mutation_protein_change = "protein-changes-test";
 const subtype_matrix = "subtype_matrix";
 
 export default {
+  legacy_msi,
   mutations_prioritized,
   mutation_protein_change,
   subtype_matrix,
