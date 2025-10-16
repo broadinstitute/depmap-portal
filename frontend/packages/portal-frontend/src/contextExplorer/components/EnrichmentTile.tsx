f3b803de745729f2dd22c0cb9fa0a5a62d75ec2e
Ali Mourey <90707472+alimourey@users.noreply.github.com>
2025-10-16 09:23:19 -0400

    Update bottom text of Enrichment Tile

--- frontend/packages/portal-frontend/src/contextExplorer/components/EnrichmentTile.tsx
+++ frontend/packages/portal-frontend/src/contextExplorer/components/EnrichmentTile.tsx
@@ -158,8 +158,8 @@ export const EnrichmentTile: React.FC<EnrichmentTileProps> = ({
           </div>
           {tileData && (
             <p className="stacked-boxplot-download-container">
-              View more contexts in the{" "}
-              <a href={tileData?.context_explorer_url}>Context Explorer</a>
+              Click a context name above to view this enrichment and additional
+              subtype-specific results in Context Explorer
             </p>
           )}
         </div>
