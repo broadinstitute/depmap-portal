47b1472fdc08cdf573437487eedee14e01aaf6a9
Randy Creasi <rcreasi@broadinstitute.org>
2025-11-17 17:07:02 -0500

    Sensitivity tab failing to load after Skyros update (#462)
    Asana:
    https://app.asana.com/1/9513920295503/project/1165651979405609/task/1211972170263118

--- frontend/packages/@depmap/cell-line-selector/src/components/CellLineListsDropdown.tsx
+++ frontend/packages/@depmap/cell-line-selector/src/components/CellLineListsDropdown.tsx
@@ -4,7 +4,6 @@ import {
   ContextSelector,
   deprecatedDataExplorerAPI,
   fetchContext,
-  isBreadboxOnlyMode,
   isNegatedContext,
   isV2Context,
   negateContext,
@@ -58,11 +57,7 @@ function CellLineListsDropdown({
       if (context && hash) {
         let labels: string[] = [];
 
-        if (isBreadboxOnlyMode) {
-          if (!isV2Context(context)) {
-            throw new Error("Can't evaluate a legacy context with Breadbox!");
-          }
-
+        if (isV2Context(context)) {
           const result = await breadboxAPI.evaluateContext(context);
           labels = result.ids;
         } else {
@@ -101,11 +96,9 @@ function CellLineListsDropdown({
       if (selectedContextHash) {
         const hashWithoutPrefix = selectedContextHash.replace("not_", "");
         try {
-          const context = await fetchContext(hashWithoutPrefix);
-
-          if (isV2Context(context)) {
-            throw new Error("V2 contexts not supported!");
-          }
+          const context = (await fetchContext(
+            hashWithoutPrefix
+          )) as DataExplorerContext;
 
           handleChange(
             selectedContextHash.startsWith("not_")
