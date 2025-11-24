da4043ac5f3939f756366ee380c8681460f17537
Ali Coppolino <90707472+alimourey@users.noreply.github.com>
2025-11-24 13:59:20 -0500

    Add related compounds tooltip (#480)

--- frontend/packages/portal-frontend/src/compound/tiles/RelatedCompoundsTile/RelatedCompoundsTile.tsx
+++ frontend/packages/portal-frontend/src/compound/tiles/RelatedCompoundsTile/RelatedCompoundsTile.tsx
@@ -3,6 +3,8 @@ import useRelatedCompoundsData from "src/compound/hooks/useRelatedCompoundsData"
 import styles from "../../styles/CorrelationTile.scss";
 import PlotSpinner from "src/plot/components/PlotSpinner";
 import { TopRelatedCompounds } from "./TopRelatedCompounds";
+import { toStaticUrl } from "@depmap/globals";
+import InfoIcon from "src/common/components/InfoIcon";
 
 interface RelatedCompoundsTileProps {
   entityLabel: string;
@@ -34,10 +36,37 @@ const RelatedCompoundsTile = ({
     return null;
   }
 
+  const customInfoImg = (
+    <img
+      style={{
+        height: "13px",
+        margin: "1px 3px 4px 3px",
+        cursor: "pointer",
+      }}
+      src={toStaticUrl("img/gene_overview/info_purple.svg")}
+      alt="description of term"
+      className="icon"
+    />
+  );
+
   return (
     <article className="card_wrapper">
       <div className="card_border container_fluid">
-        <h2 className="no_margin cardtitle_text">Related Compounds</h2>
+        <h2 className="no_margin cardtitle_text">
+          Related Compounds{" "}
+          <InfoIcon
+            target={customInfoImg}
+            popoverContent={
+              <p>
+                {
+                  "In this heatmap, the vertical axis are the compounds which share a target with the currently selected compound. The horizontal axis is the targets as deactivated by either CRISPR or RNAi. Each cell in the heatmap is the Pearson correlation computed comparing the viability effect (as measured by log2(AUC) ) and the Gene Effect observed from disabling the same gene across shared lines tested in both assays. The color also encodes the correlation value with red indicating positive correlation and blue indicating negative correlation. Those cells which are blank are cases where the correlation was filtered out due being weaker than the top 250 correlates for the given compound."
+                }
+              </p>
+            }
+            popoverId={`related-compounds-tile-popover`}
+            trigger={["hover", "focus"]}
+          />
+        </h2>
         <div className="card_padding">
           {isLoading && <PlotSpinner />}
           {hasError && !isLoading && (
