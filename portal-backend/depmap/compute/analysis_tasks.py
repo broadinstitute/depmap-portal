ce536e5f02dc170ecc161ee234a5ee02b3fd4daa
Sarah Wessel Whitaker <swessel@broadinstitute.org>
2025-12-01 14:47:19 -0500

    Update comment

--- portal-backend/depmap/compute/analysis_tasks.py
+++ portal-backend/depmap/compute/analysis_tasks.py
@@ -66,7 +66,7 @@ def run_custom_analysis(
     :param user_id: The user id that should be used to verify they have access to see the matrix
     :return:
     """
-    # TODO: delete this entire method now
+    # DEPRECATED: this function is now only being used in tests.
     with assume_user(user_id):
         feature_labels = data_access.get_dataset_feature_labels(dataset_id)
         features = get_features(dataset_id, feature_labels)
