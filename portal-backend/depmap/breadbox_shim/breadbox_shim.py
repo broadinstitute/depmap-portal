f83e3a8fe66655f250919b1956c4531d68507be0
Sarah Wessel Whitaker <swessel@broadinstitute.org>
2025-12-01 14:21:55 -0500

    Remove remaining parts of endpoint definition

--- portal-backend/depmap/breadbox_shim/breadbox_shim.py
+++ portal-backend/depmap/breadbox_shim/breadbox_shim.py
@@ -77,57 +77,6 @@ def get_features_calculated_value_lists(
     return feature_values, axis_labels, feature_labels, slice_ids
 
 
-def run_custom_analysis(
-    analysis_type: str,
-    dataset_slice_id: str,
-    slice_query: Optional[SliceQuery],
-    vector_variable_type: str,
-    query_cell_lines: Optional[list[str]],
-    query_values: Optional[list[Any]],
-) -> ComputeResponse:
-    """
-    Run custom analysis in breadbox using a breadbox dataset.
-    The selected feature may either be a breadbox feature (specified with the given query_id)
-    or a legacy portal feature (specified with the given feature_data).
-    Return a task status.
-    """
-    bb_dataset_id = remove_breadbox_prefix(dataset_slice_id)
-    if slice_query:
-        # Temporary hack: for now, this slice query ALWAYS specifies a feature by label.
-        # This should always be true for our current feature selection component.
-        # Soon, this "breadbox_shim" should be removed entirely, along with this hack.
-        assert slice_query.identifier_type == "feature_label"
-
-        # Hack part 2: Custom analysis in Breadbox was set up to take a feature's given ID.
-        # We have the label here and need to use that to load the given ID.
-        query_dataset_id = parse_breadbox_slice_id(slice_query.dataset_id).dataset_id
-        all_dataset_features = extensions.breadbox.client.get_dataset_features(
-            query_dataset_id
-        )
-        feature_id = None
-        for bb_feature in all_dataset_features:
-            if bb_feature["label"] == slice_query.identifier:
-                feature_id = bb_feature["id"]
-        assert (
-            feature_id is not None
-        ), f"Unexpected feature label passed to breadbox custom analysis: '{slice_query.identifier}'"
-
-    else:
-        feature_id = ""
-        query_dataset_id = ""
-    bb_task_status = extensions.breadbox.client.compute_univariate_associations(
-        analysis_type=analysis_type,
-        dataset_id=bb_dataset_id,
-        query_feature_id=feature_id,
-        query_dataset_id=query_dataset_id,
-        vector_variable_type=vector_variable_type,
-        query_cell_lines=query_cell_lines,
-        query_values=query_values,
-    )
-    bb_task_status.id = f"breadbox/{bb_task_status.id}"
-    return bb_task_status
-
-
 def get_breadbox_task_status(id: str):
     """
     Get the task status for a given breadbox task id.
--- portal-backend/depmap/compute/views.py
+++ portal-backend/depmap/compute/views.py
@@ -1,18 +1,11 @@
 import logging
 
-from flask import abort, Blueprint, render_template, request
+from flask import Blueprint, render_template, request
 from flask_restplus import Api, Resource
-import pandas as pd
 
-from depmap.interactive import interactive_utils
-from depmap.breadbox_shim import breadbox_shim
-from depmap.celery_task.utils import (
-    format_taskless_error_message,
-    task_response_model,
-)
+from depmap.celery_task.utils import task_response_model
 from depmap.extensions import csrf_protect, restplus_handle_exception
 from depmap_compute.models import AnalysisType
-from depmap_compute.slice import slice_id_to_slice_query
 
 blueprint = Blueprint(
     "compute", __name__, url_prefix="/compute", static_folder="../static"
@@ -37,124 +30,3 @@ log = logging.getLogger(__name__)
 def ui():
     return render_template("compute/ui.html")
 
-
-@restplus.route("/compute_univariate_associations")
-class ComputeUnivariateAssociations(Resource):
-    @restplus.doc(
-        description="Runs univariate associations",
-        params={
-            "analysisType": {
-                "description": 'Either "pearson", "association", or "two_class"'
-            },
-            "datasetId": {"description": "the dataset to search for association"},
-            "queryCellLines": {
-                "description": "A list of depmap ids, such that the calculation will only use values for these lines. Must be provided for two class, optional for association.",
-                "type": "list",
-            },
-            "queryId": {
-                "description": "Associations only, slice id accepted by the interactive module for the profile to search for (must be None if queryValues is provided, aka two class)"
-            },
-            "vectorVariableType": {
-                "description": 'Association and two class only. Either "dependent" or "independent". Indicates whether the vector is the dependent or independent variable'
-            },
-            "queryValues": {
-                "description": "Two class only, list of labels 'in' or 'out', a value for each cell line in the query. (Must be None if queryId is provided)",
-                "type": "list",
-            },
-        },
-    )
-    @restplus.marshal_with(restplus.model("Task", task_response_model,))
-    def post(self):
-        dataset_id = request.json.get("datasetId")
-        query_id = request.json.get("queryId")
-        vector_variable_type = request.json.get("vectorVariableType")
-        query_cell_lines = request.json.get("queryCellLines")
-        query_values = request.json.get("queryValues")
-
-        analysis_type = {
-            "pearson": AnalysisType.pearson,
-            "association": AnalysisType.association,
-            "two_class": AnalysisType.two_class,
-        }[request.json.get("analysisType")]
-
-        # verify fields were populated in their correct combinations for the appropriate analysis
-        assert dataset_id is not None
-
-        # check vector_variable_type and assign vector_is_dependent
-        if analysis_type == AnalysisType.pearson:
-            assert vector_variable_type is None
-            vector_is_dependent = None
-        else:
-            assert (
-                vector_variable_type == "dependent"
-                or vector_variable_type == "independent"
-            ), "Unexpected vector_variable_type, is {}. Must be 'dependent' or 'independent'".format(
-                vector_variable_type
-            )
-            vector_is_dependent = vector_variable_type == "dependent"
-
-        # check other parameters
-        if (
-            analysis_type == AnalysisType.pearson
-            or analysis_type == AnalysisType.association
-        ):
-            # association and pearson have the same parameters, except for dependent/independent which is handled above
-
-            assert query_id
-            # may optionally provide query cell lines
-            assert not query_values
-        elif analysis_type == AnalysisType.two_class:
-            assert not query_id
-            assert query_cell_lines
-            assert query_values
-        else:
-            raise ValueError("Unexpected analysis type {}".format(analysis_type))
-
-        # Parse the slice ID if one was provided
-        if query_id:
-            slice_query = slice_id_to_slice_query(query_id)
-            slice_query_is_from_breadbox = slice_query.dataset_id.startswith("breadbox/")
-        else:
-            slice_query = None
-            slice_query_is_from_breadbox = False
-
-        # Forward requests to breadbox a breadbox dataset is requested
-        if not dataset_id.startswith("breadbox/"):
-            raise ValueError("TODO: return 501 not implemented ")
-        else:
-            # If the query slice is from a legacy dataset, load it now and pass the values to breadbox
-            # The query_cell_lines parameter needs to be the same order/length as the query_values when passed to breadbox.
-            if slice_query and not slice_query_is_from_breadbox:
-                # In this specific case, it's important to avoid the data_access interface because 
-                # breadbox legacy-dataset aliases don't work for lookups by entity_id 
-                # (which is still the slice format used by celfie/genomic associations)
-                legacy_data_slice: pd.Series = interactive_utils.get_row_of_values_from_slice_id(
-                    query_id
-                )
-                if query_cell_lines is not None:
-                    # When the cell lines have been filtered by the user,
-                    # the legacy feature series also needs to be filtered before being passed to breadbox.
-                    feature_cell_lines = legacy_data_slice.index.tolist()
-                    unordered_cell_lines_interesection = list(
-                        set(query_cell_lines).intersection(set(feature_cell_lines))
-                    )
-                    if len(unordered_cell_lines_interesection) == 0:
-                        return format_taskless_error_message(
-                            "No cell lines in common between query and dataset searched"
-                        )
-                    legacy_data_slice = legacy_data_slice.loc[
-                        unordered_cell_lines_interesection
-                    ]
-                query_values = legacy_data_slice.tolist()
-                query_cell_lines: list[
-                    str
-                ] = legacy_data_slice.index.tolist()  # pyright: ignore
-                slice_query = None
-            return breadbox_shim.run_custom_analysis(
-                analysis_type=analysis_type,
-                dataset_slice_id=dataset_id,
-                slice_query=slice_query,
-                vector_variable_type=vector_variable_type,
-                query_cell_lines=query_cell_lines,
-                query_values=query_values,
-            )
