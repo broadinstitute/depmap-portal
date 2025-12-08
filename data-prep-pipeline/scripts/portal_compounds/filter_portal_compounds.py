87a09eebb2437b92cdace732bc54e9860c7669dc
Philip Montgomery <pmontgom@broadinstitute.org>
2025-12-08 08:26:39 -0500

    WIP changes for filtering portal compounds which were manually run for 25Q4

--- data-prep-pipeline/scripts/portal_compounds/filter_portal_compounds.py
+++ data-prep-pipeline/scripts/portal_compounds/filter_portal_compounds.py
@@ -1,7 +1,8 @@
+import collections
 import argparse
 import pandas as pd
 from taigapy import create_taiga_client_v3
-from typing import Set
+from typing import Set, Dict, List
 
 
 def filter_sample_ids(sample_ids: str, brd_ids: Set[str]) -> str:
@@ -30,6 +31,7 @@ def filter_portal_compounds(df: pd.DataFrame, brd_ids: Set[str]) -> pd.DataFrame
     specified rules. Rows with empty SampleIDs are removed.
     """
     df = df.copy()
+
     # Use apply to filter SampleIDs
     df["SampleIDs"] = df["SampleIDs"].apply(
         lambda x: filter_sample_ids(x, brd_ids) if pd.notna(x) else ""
@@ -43,84 +45,126 @@ def filter_portal_compounds(df: pd.DataFrame, brd_ids: Set[str]) -> pd.DataFrame
     return df_filtered
 
 
-if __name__ == "__main__":
+def check_for_all_ids(df, brd_id_to_sources: Dict[str, List[str]]):
+    # compute the union of all SampleIDs
+    remaining_sample_ids = set()
+    for sample_ids in df["SampleIDs"]:
+        remaining_sample_ids.update(sample_ids.split(";"))
+
+    # now identify which samples aren't in the final version, but _were_ in the original compound lists
+    missing_count = 0
+    for brd_id, sources in brd_id_to_sources.items():
+        if brd_id not in remaining_sample_ids:
+            print(f"Could not find {brd_id} which was referenced in {sources}")
+            missing_count += 1
+    assert missing_count < 120, f"{missing_count} missing IDs"
+
+
+def main():
     parser = argparse.ArgumentParser(
         description="Filter portal compounds data and upload to Taiga."
     )
     parser.add_argument(
-        "repsdrug_matrix_taiga_id",
-        help="Taiga ID of PRISM Repurposing primary screen data",
+        "master_portal_compounds_taiga_id",
+        help="Taiga ID of the full merged list all of compounds",
     )
     parser.add_argument(
-        "repsdrug_auc_matrix_taiga_id",
-        help="Taiga ID of PRISM Repurposing secondary screen data",
+        "--index-of-matrix",
+        help="Matrix to look for sample IDs within",
+        action="append",
     )
     parser.add_argument(
-        "portal_compounds_taiga_id", help="Taiga ID of portal compounds data"
-    )
-    parser.add_argument(
-        "--prism_oncology_reference_auc_matrix_taiga_id",
-        help="Taiga ID of the PRISMOncologyReferenceLog2AUCMatrix (optional)",
-        default=None,
-    )
-    parser.add_argument(
-        "--prism_oncology_reference_seq_log2_auc_matrix_taiga_id",
-        help="Taiga ID of the PRISMOncologyReferenceSeqLog2AUCMatrix (optional)",
-        default=None,
+        "--sample-id-of-table",
+        help="Table to look for sample IDs within 'SampleID' column",
+        action="append",
     )
     parser.add_argument("output", help="Path to write the output")
 
     args = parser.parse_args()
     tc = create_taiga_client_v3()
 
-    print("Getting PRISM Repurposing primary screen data...")
-    repsdrug_matrix = tc.get(args.repsdrug_matrix_taiga_id)
-    assert not repsdrug_matrix.index.empty, "repsdrug_matrix index is empty"
+    matrices = args.index_of_matrix or []
+    tables = args.sample_id_of_table or []
 
-    print("Getting PRISM Repurposing secondary screen data...")
-    repsdrug_auc = tc.get(args.repsdrug_auc_matrix_taiga_id)
+    brd_ids = set()
+    brd_sources = collections.defaultdict(lambda: [])
 
-    print("Getting portal compounds data...")
-    portal_compounds_df = tc.get(args.portal_compounds_taiga_id)
-    assert not portal_compounds_df.empty, "portal_compounds_df is empty"
+    def add_brd_ids(ids, source):
+        for id in ids:
+            if id.startswith("PRC-"):
+                id = "BRD:" + id
+            if id.startswith("BRD:"):
+                brd_ids.add(id)
+                brd_sources[id].append(source)
+            else:
+                raise Exception(f"Invalid BRD ID: {id} in {source}")
 
-    if (
-        args.prism_oncology_reference_auc_matrix_taiga_id is None
-        or args.prism_oncology_reference_auc_matrix_taiga_id.startswith("public")
-    ):
-        oncrefauc_matrix = pd.DataFrame()
-    else:
-        print("Getting oncref AUC matrix data...")
-        oncrefauc_matrix = tc.get(args.prism_oncology_reference_auc_matrix_taiga_id)
-        assert not oncrefauc_matrix.columns.empty, "oncrefauc_matrix columns are empty"
+    for matrix_taiga_id in matrices:
+        print(f"Getting {matrix_taiga_id} for index")
+        matrix = tc.get(matrix_taiga_id)
+        add_brd_ids(matrix.index, matrix_taiga_id)
 
-    if (
-        args.prism_oncology_reference_seq_log2_auc_matrix_taiga_id is None
-        or args.prism_oncology_reference_seq_log2_auc_matrix_taiga_id.startswith(
-            "public"
-        )
-    ):
-        oncref_seq_log2_auc_matrix = pd.DataFrame()
-    else:
-        print("Getting oncref seq log2 AUC matrix data...")
-        oncref_seq_log2_auc_matrix = tc.get(
-            args.prism_oncology_reference_seq_log2_auc_matrix_taiga_id
-        )
-        assert (
-            not oncref_seq_log2_auc_matrix.columns.empty
-        ), "oncref_seq_log2_auc_matrix columns are empty"
+    for table_taiga_id in tables:
+        print(f"Getting {table_taiga_id} for SampleID column")
+        table = tc.get(table_taiga_id)
+        add_brd_ids(table["SampleID"], table_taiga_id)
 
-    print("Computing IDs for filtering...")
-    brd_ids = (
-        set(repsdrug_matrix.index)
-        .union(repsdrug_auc.index)
-        .union(["BRD:" + x for x in oncrefauc_matrix.columns])
-        .union(["BRD:" + x for x in oncref_seq_log2_auc_matrix.columns])
-    )
+    portal_compounds_df = tc.get(args.master_portal_compounds_taiga_id)
 
+    #
+    # compound_id_by_sample_id = {}
+    # for row in portal_compounds_df.to_records():
+    #     for sample_id in row['SampleIDs'].split(";"):
+    #         compound_id_by_sample_id[sample_id] = row['CompoundID']
+    #
+    # print("Getting PRISM Repurposing primary screen data...")
+    # repsdrug_matrix = tc.get(args.repsdrug_matrix_taiga_id)
+    # assert not repsdrug_matrix.index.empty, "repsdrug_matrix index is empty"
+    #
+    # print("Getting PRISM Repurposing secondary screen data...")
+    # repsdrug_auc = tc.get(args.repsdrug_auc_matrix_taiga_id)
+    #
+    #
+    # print("Getting oncref AUC matrix data...")
+    # oncrefauc_matrix = tc.get(args.prism_oncology_reference_auc_matrix_taiga_id)
+    # assert not oncrefauc_matrix.columns.empty, "oncrefauc_matrix columns are empty"
+    #
+    # print("Getting oncref seq log2 AUC matrix data...")
+    # oncref_seq_log2_auc_matrix = tc.get(
+    #     args.prism_oncology_reference_seq_log2_auc_matrix_taiga_id
+    # )
+    # assert (
+    #     not oncref_seq_log2_auc_matrix.columns.empty
+    # ), "oncref_seq_log2_auc_matrix columns are empty"
+    #
+    # print("Computing IDs for filtering...")
+    # brd_id_with_sources = collections.defaultdict(list)
+    #
+    # compound_ids = set( list(repsdrug_matrix.columns) + list(repsdrug_auc.columns) )
+    #
+    # source_to_brds = {
+    #     "oncrefauc_matrix": ["BRD:" + x for x in oncrefauc_matrix.columns],
+    #     "oncref_seq_log2_auc_matrix": ["BRD:" + x for x in oncref_seq_log2_auc_matrix.columns]
+    # }
+    #
+    # for name, ids in source_to_brds.items():
+    #     for id in ids:
+    #         brd_id_with_sources[id].append(name)
+    #         cid = compound_id_by_sample_id.get(id)
+    #         if cid is None:
+    #             print("missing id", id)
+    #             continue
+    #         compound_ids.add(cid)
+    #
     print("Filtering portal compounds data...")
     portal_compounds_filtered = filter_portal_compounds(portal_compounds_df, brd_ids)
     print("Filtered portal compounds data")
 
+    check_for_all_ids(portal_compounds_filtered, brd_sources)
+
     if portal_compounds_filtered is not None:
         portal_compounds_filtered.to_csv(args.output, index=False)
+
+
+if __name__ == "__main__":
+    main()
