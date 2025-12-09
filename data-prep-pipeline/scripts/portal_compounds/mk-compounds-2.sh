eb041da7c3ec57c2c9d784e5d7e453a1fd26c1a3
Philip Montgomery <pmontgom@broadinstitute.org>
2025-12-09 17:35:25 -0500

    fixed input datasets for filter script

--- data-prep-pipeline/scripts/portal_compounds/mk-compounds-2.sh
+++ data-prep-pipeline/scripts/portal_compounds/mk-compounds-2.sh
@@ -1,8 +1,8 @@
 python filter_portal_compounds.py \
   compound-metadata-de37.51/PortalCompounds \
-  --sample-id-of-table internal-25q3-b129.169/PRISMOncologyReferenceCompoundList \
-  --sample-id-of-table internal-25q3-b129.169/PRISMOncologyReferenceSeqCompoundList \
+  --sample-id-of-table oncref-25q4-all-files-4991.7/PRISMOncologyReferenceLumCompoundList \
+  --sample-id-of-table oncref-25q4-all-files-4991.7/PRISMOncologyReferenceSeqCompoundList \
   --sample-id-of-table processed-repurposing-secondary-e3aa.3/REPURPOSINGLog2ViabilityCollapsedConditions \
-  --index-of-matrix repurposing-public-23q2-341f.8/Repurposing_Public_23Q2_Extended_Primary_Data_Matrix \
-  out2.csv
+  --index-of-matrix repurposing-23q2-a803.4/Repurposing_23Q2_Extended_Primary_Data_Matrix_Per_Compound \
+  out3.csv
 
