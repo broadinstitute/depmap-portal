c77260cfb3b818bb0a0017c31242ccd6222f71cd
Sarah Wessel Whitaker <swessel@broadinstitute.org>
2025-12-16 17:14:25 -0500

    Remove entire depmap_compute module

--- depmap-compute/depmap_compute/models.py
+++
@@ -1,8 +0,0 @@
-from enum import Enum
-
-
-class AnalysisType(str, Enum):
-    # this subclasses string in order to be json serializable
-    pearson = "pearson"
-    association = "association"
-    two_class = "two_class"
