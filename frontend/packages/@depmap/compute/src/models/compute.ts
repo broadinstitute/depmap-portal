844bf0270e1c84e09b733cd632e9295646f30b57
Randy Creasi <rcreasi@broadinstitute.org>
2026-01-06 16:41:44 -0500

    remove legacy code

--- frontend/packages/@depmap/compute/src/models/compute.ts
+++ frontend/packages/@depmap/compute/src/models/compute.ts
@@ -4,23 +4,6 @@ export type AssociationOrPearsonAnalysisType = "association" | "pearson";
 export type AnalysisType = AssociationOrPearsonAnalysisType | "two_class";
 export type VariableType = "dependent" | "independent";
 
-export type SelectNSOption = {
-  label: string;
-  value: string;
-};
-
-export type QuerySelections = {
-  // these numbers refer to positions in the model grid
-  //    1  2
-  // 3  4  5
-
-  1: React.ReactNode;
-  2?: React.ReactNode;
-  3: React.ReactNode;
-  4?: React.ReactNode;
-  5?: React.ReactNode;
-};
-
 export interface UnivariateAssociationsParams {
   analysisType: AnalysisType;
   datasetId: string;
@@ -58,42 +41,4 @@ export interface ComputeResponseRow {
 
 export interface ComputeResponse extends CeleryTask {
   result: ComputeResponseResult; // not returned if the job failed
-}
-
-export interface CommonQueryProps {
-  analysisType: AnalysisType; // only used for association and pearson
-  renderBodyFooter: (
-    querySelections: QuerySelections,
-    queryIsValid: boolean,
-    sendQuery: () => void
-  ) => React.ReactNode;
-  sendQueryGeneric: (
-    runCustomAnalysis: () => Promise<ComputeResponse>,
-    onSuccess: (
-      response: ComputeResponse,
-      onResultsComplete?: (response: ComputeResponse) => void
-    ) => void
-  ) => void;
-  onSuccessGeneric: (
-    response: ComputeResponse,
-    onResultsComplete: (response: any) => void
-  ) => void;
-  queryInfo: {
-    cellLineData: Map<string, { displayName: string }>; // map with key = depmapId
-    datasets:
-      | SelectNSOption[]
-      | Array<
-          // HACK: `data_type` and `priority` were not available in the legacy
-          // portal. THese will only be present if `isBreadboxOnlyMode` is true.
-          SelectNSOption & { data_type?: string; priority?: number }
-        >;
-  };
-  onAssociationResultsComplete: (
-    numCellLinesUsed: number,
-    result: ComputeResponseResult,
-    queryVectorId: string,
-    overrideColorState: string,
-    overrideFilterState: string,
-    analysisType: AnalysisType
-  ) => void;
 }
