be4ef8c50587447a79a86b4b2becd0d268669576
Randy Creasi <rcreasi@broadinstitute.org>
2026-01-07 11:05:09 -0500

    Rebuild custom analysis UI (#524)
    Asana task:
    https://app.asana.com/1/9513920295503/project/1165651979405609/task/1212211283468502?focus=true

--- frontend/packages/@depmap/compute/index.ts
+++ frontend/packages/@depmap/compute/index.ts
@@ -1,20 +1,12 @@
 export { CustomAnalysisResult } from "./src/components/CustomAnalysisResult";
-export { DatasetSelect } from "./src/components/DatasetSelect";
 export { FileUpload } from "./src/components/FileUpload";
-export { TwoClassQuery } from "./src/components/TwoClassQuery";
-export { default as AssociationPearsonQuery } from "./src/components/AssociationPearsonQuery";
-
-export type { Link } from "./src/models/legacy";
 
 export type {
   AnalysisType,
-  CommonQueryProps,
   ComputeResponse,
   ComputeResponseResult,
   ComputeResponseRow,
-  QuerySelections,
   UnivariateAssociationsParams,
-  VariableType,
 } from "./src/models/compute";
 
 export type {
