59d8beea9ecf11ff410e29a258655b4ad73d147e
Ali Mourey <90707472+alimourey@users.noreply.github.com>
2026-02-04 14:42:57 -0500

    Delete some older no longer used code

--- frontend/packages/@depmap/api/src/legacyPortalAPI/resources/compound.ts
+++ frontend/packages/@depmap/api/src/legacyPortalAPI/resources/compound.ts
@@ -1,27 +1,9 @@
 import {
   CompoundDoseCurveData,
-  CurveParams,
-  CurvePlotPoints,
   DRCDatasetOptions,
   SensitivityTabSummary,
 } from "@depmap/types";
-import { uri } from "../../uriTemplateTag";
 import { getJson } from "../client";
-
-export function getDoseResponsePoints(
-  datasetName: string,
-  depmapId: string,
-  compoundLabel: string
-) {
-  return getJson<{
-    curve_params: Array<CurveParams>;
-    points: Array<CurvePlotPoints>;
-  }>(uri`/compound/dosecurve/${datasetName}/${depmapId}/${compoundLabel}`);
-}
-
-export function getDoseResponseTable(datasetName: string, xrefFull: string) {
-  return getJson<any>(uri`/compound/dosetable/${datasetName}/${xrefFull}`);
-}
 
 export function getCompoundDoseCurveData(
   compoundId: string,
--- portal-backend/depmap/compound/views/index.py
+++ portal-backend/depmap/compound/views/index.py
@@ -1,19 +1,15 @@
 from itertools import groupby
-import math
 import os
 import tempfile
-from typing import Any, List, Optional
+from typing import List
 import zipfile
-from depmap.compound import legacy_utils
 from depmap.compound.utils import (
-    find_compound_dataset,
     get_compound_dataset_with_name_and_priority,
     dataset_exists_with_compound_in_auc_and_rep_datasets,
 )
 
 from depmap.context_explorer.models import ContextExplorerDatasets
 from depmap.extensions import memoize_without_user_permissions
-import numpy as np
 import pandas as pd
 from flask import (
     Blueprint,
@@ -27,15 +23,10 @@ from flask import (
 )
 
 from depmap import data_access
-from depmap.data_access.models import MatrixDataset
-from depmap.cell_line.models_new import DepmapModel
 from depmap.compound.models import (
     Compound,
-    CompoundDoseReplicate,
-    CompoundExperiment,
     DRCCompoundDataset,
     DRCCompoundDatasetWithNamesAndPriority,
-    DoseResponseCurve,
     drc_compound_datasets,
 )
 from depmap.compound.views.executive import (
@@ -43,14 +34,11 @@ from depmap.compound.views.executive import (
     get_predictive_models_for_compound,
 )
 from depmap.dataset.models import Dataset, DependencyDataset
-from depmap.entity.views.index import format_celfie
 from depmap.enums import DependencyEnum
-from depmap.partials.matrix.models import ColMatrixIndex
 from depmap.predictability.models import PredictiveFeatureResult, PredictiveModel
 from depmap.predictability.utilities import (
     get_predictability_input_files_downloads_link,
 )
-from depmap.settings.shared import DATASET_METADATA
 from depmap.utilities.sign_bucket_url import get_signed_url
 
 blueprint = Blueprint(
@@ -81,9 +69,6 @@ def _get_compound_page_template_parameters(name):
         compound.compound_id
     )
     has_datasets = len(compound_datasets) != 0
-    sensitivity_tab_compound_summary = get_sensitivity_tab_info(
-        compound.entity_id, compound_datasets
-    )
 
     dose_curve_options_new = get_new_dose_curves_tab_drc_options(
         compound_label=compound.label, compound_id=compound.compound_id
@@ -132,7 +117,6 @@ def _get_compound_page_template_parameters(name):
         name=name,
         compound_id=compound.compound_id,
         compound_aliases=compound_aliases,
-        summary=sensitivity_tab_compound_summary,
         has_predictability=has_predictability,
         predictability_custom_downloads_link=get_predictability_input_files_downloads_link(),
         predictability_methodology_link=get_signed_url(
@@ -170,60 +154,6 @@ def view_compound(name):
 
     # but this template has a call to is_mobile which cannot safely be cached
     return render_template("compounds/index.html", **template_parameters)
-
-
-def format_summary_option(dataset, entity, label):
-    option = {
-        "label": label,
-        "id": "{}_{}".format(
-            dataset.name.name, entity.entity_id
-        ),  # used for uniqueness
-        "dataset": dataset.name.name,
-        "entity": entity.entity_id,
-    }
-    return option
-
-
-def format_dose_curve_options(compound_experiment_and_datasets):
-    """
-    Used for jinja rendering of the dose curve tab
-    """
-    dose_curve_options = []
-    for compound_experiment, dataset in compound_experiment_and_datasets:
-        # if has dose curve information
-        if (
-            dataset.get_dose_replicate_enum()
-            and len(
-                CompoundDoseReplicate.get_all_with_compound_experiment_id(
-                    compound_experiment.entity_id
-                )
-            )
-            > 0
-        ):
-            dose_curve_option = format_dose_curve_option(
-                dataset,
-                compound_experiment,
-                "{} {}".format(compound_experiment.label, dataset.display_name),
-            )
-            dose_curve_options.append(dose_curve_option)
-    return dose_curve_options
-
-
-def format_dose_curve_option(dataset, compound_experiment, label):
-    option = format_summary_option(dataset, compound_experiment, label)
-    option.update(
-        {
-            "dose_replicate_dataset": dataset.get_dose_replicate_enum().name,
-            "auc_dataset_display_name": dataset.display_name,
-            "compound_label": compound_experiment.label,
-            "compound_xref_full": compound_experiment.xref_full,
-            "dose_replicate_level_yunits": DATASET_METADATA[
-                dataset.get_dose_replicate_enum()
-            ].units,
-        }
-    )
-
-    return option
 
 
 def get_heatmap_tab_drc_options(
@@ -304,159 +234,6 @@ def get_corr_analysis_options_if_available(
 @blueprint.route("/compoundUrlRoot")
 def get_compound_url_route():
     return jsonify(url_for("compound.view_compound", name=""))
-
-
-# for testing:  http://127.0.0.1:5000/depmap-xqa/compound/dosecurve/CTRP_dose_replicate/ACH-000425/CTRP:1788
-@blueprint.route("/dosecurve/<dataset_name>/<depmap_id>/<compound_label>")
-def fetch_dose_curve(dataset_name, depmap_id, compound_label):
-    dose_response_curve = format_dose_curve(dataset_name, depmap_id, compound_label)
-    return jsonify(dose_response_curve)
-
-
-def format_dose_curve(dataset_name, depmap_id, xref_full):
-    # first, fetch the points of the dose response curve
-    # use dataset_name to get the Dataset by name
-    dataset = Dataset.get_dataset_by_name(dataset_name)
-    assert dataset
-
-    # get matrix associated with the dataset
-    matrix = dataset.matrix
-
-    cell_line = DepmapModel.get_by_model_id(depmap_id)
-
-    # use xref_full to get the appropriate CompoundExperiment
-    compound_experiment = CompoundExperiment.get_by_xref_full(xref_full)
-
-    # get all CompoundDoseReplicate objects associated with CompoundExperiment
-    compound_dose_replicates = CompoundDoseReplicate.get_all_with_compound_experiment_id(
-        compound_experiment.entity_id
-    )
-    compound_dose_replicates = [
-        dose_rep
-        for dose_rep in compound_dose_replicates
-        if DependencyDataset.has_entity(dataset.name, dose_rep.entity_id)
-    ]
-
-    # call the get_values_by_entities_and_depmap_id function in matrix, passing in entities and depmap id
-    viabilities = matrix.get_values_by_entities_and_depmap_id(
-        entities=compound_dose_replicates, depmap_id=cell_line.depmap_id
-    )
-
-    # points only contains viability -- we need to add on dose, isMasked, and replicate ourselves
-    assert len(compound_dose_replicates) == len(viabilities)
-    points = []
-    for i in range(len(viabilities)):
-        if (viabilities[i] is not None) & (not math.isnan(viabilities[i])):
-            points.append(
-                {
-                    "dose": compound_dose_replicates[i].dose,
-                    "viability": viabilities[i].item(),
-                    "isMasked": compound_dose_replicates[i].is_masked,
-                    "replicate": compound_dose_replicates[i].replicate,
-                }
-            )
-
-    # fetch the dose response curve parameters using cell line name and compound experiment to find the appropriate DoseResponseCurve
-    curve_objs = DoseResponseCurve.query.filter(
-        DoseResponseCurve.compound_exp == compound_experiment,
-        DoseResponseCurve.cell_line == cell_line,
-    ).all()
-
-    curve_params = []
-
-    for curve in curve_objs:
-        curve_param = {
-            "ec50": curve.ec50,
-            "slope": curve.slope,
-            "lowerAsymptote": curve.lower_asymptote,
-            "upperAsymptote": curve.upper_asymptote,
-        }
-        curve_params.append(curve_param)
-
-    dose_response_curve = {"points": points, "curve_params": curve_params}
-
-    return dose_response_curve
-
-
-@blueprint.route("/dosetable/<dataset_name>/<xref_full>")
-def dose_table(dataset_name, xref_full):
-    """
-    Return table of dose responses for different doses by cell line, plus depmap id
-    and display name for the cell lines.
-    :param dataset_name: enum name for the dose replicate dataset
-    :return: a json structure like
-        {
-           "ACH-000552":{
-              "0-0045":0.9473772049, # "0-0045" refers to the dose but with "." replaced with "-". This thus corresponds to 0.0045
-              "0-29":0.8894940615,
-              "2-3":0.68013376,
-              "9-2":0.1122418195,
-              "cell_line_display_name":"HT29",
-              "auc":0.8729583436,
-           },
-           "ACH-000279":{
-              ...
-    """
-    # first, fetch the points of the dose response curve
-    # use dataset_name to get the Dataset by name
-    dataset = Dataset.get_dataset_by_name(dataset_name, must=True)
-
-    # get matrix associated with the dataset
-    matrix = dataset.matrix
-
-    # use xref_full to get the appropriate CompoundExperiment
-    compound_experiment = CompoundExperiment.get_by_xref_full(xref_full)
-
-    # get all CompoundDoseReplicate objects associated with CompoundExperiment
-    compound_dose_replicates = CompoundDoseReplicate.get_all_with_compound_experiment_id(
-        compound_experiment.entity_id
-    )
-    compound_dose_replicates = [
-        dose_rep
-        for dose_rep in compound_dose_replicates
-        if DependencyDataset.has_entity(dataset.name, dose_rep.entity_id)
-    ]
-
-    matrix_col_index: List[ColMatrixIndex] = matrix.col_index.order_by(
-        ColMatrixIndex.index
-    ).all()
-    # We need to filter out cols that exist in the underlying HDF5 file, but that were
-    # not loaded as ColMatrixIndexes
-    table_indices = [ci.index for ci in matrix_col_index]
-
-    table = np.array(
-        [matrix.get_values_by_entity(cpr.entity_id) for cpr in compound_dose_replicates]
-    )
-
-    df = pd.DataFrame(
-        table[:, table_indices], columns=[ci.depmap_id for ci in matrix_col_index]
-    )
-
-    # The React component BaseTable has issues with periods in column names
-    # "{:.15f}".format(cpr.dose) to fix UI bug caused by scientific notation. 5e-05 was being
-    # parsed to 5μM instead of 0.000050 μM
-    df["dose"] = [
-        str("{:.15f}".format(cpr.dose)).replace(".", "-")
-        for cpr in compound_dose_replicates
-    ]
-
-    if len(df) > 0:
-        df = df.groupby("dose").mean()
-
-    df = df.T
-
-    cell_line_names = DepmapModel.get_cell_line_display_names(df.index)
-
-    df = df.merge(cell_line_names, left_index=True, right_index=True)
-
-    #### AUC
-    auc_data = get_auc_data(dataset_name, compound_experiment)
-
-    if auc_data is not None:
-        df = df.merge(auc_data, left_index=True, right_index=True, how="left")
-
-    df = df.T
-    return df.to_json()
 
 
 def get_auc_data(dataset_name, compound_experiment):
