59bb391ccb2ce9bbef98bffe8c02f46e32c1f4ce
Sarah Wessel Whitaker <swessel@broadinstitute.org>
2026-02-09 13:33:31 -0500

    Remove old datasets & associations from legacy DB (#582)

--- portal-backend/loader/dataset_loader/biomarker_loader.py
+++ portal-backend/loader/dataset_loader/biomarker_loader.py
@@ -43,7 +43,6 @@ GENERIC_ENTITY_BIOMARKER_ENUMS = [
     BiomarkerDataset.BiomarkerEnum.metabolomics,
     BiomarkerDataset.BiomarkerEnum.crispr_confounders,
     BiomarkerDataset.BiomarkerEnum.rnai_confounders,
-    BiomarkerDataset.BiomarkerEnum.rep1m_confounders,
     BiomarkerDataset.BiomarkerEnum.oncref_confounders,
     BiomarkerDataset.BiomarkerEnum.oncref_seq_confounders,
     BiomarkerDataset.BiomarkerEnum.rep_all_single_pt_confounders,
