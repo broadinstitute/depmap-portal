6858741bc13ed076b956dc4b9fb237068f280bc4
Philip Montgomery <pmontgom@broadinstitute.org>
2026-02-14 13:15:55 +0000

    fixed preprocessing for daintree

--- pipeline/analysis-pipeline/predictability/scripts/daintree_preprocess.py
+++ pipeline/analysis-pipeline/predictability/scripts/daintree_preprocess.py
@@ -1,2 +1,10 @@
+columns_to_drop = ['SequencingID', 'ModelConditionID', 'IsDefaultEntryForModel',
+       'IsDefaultEntryForMC']
+
 def index_by_model(df):
-    raise Exception("unimp")
+    if 'IsDefaultEntryForModel' in df:
+        print("Detected matrix which needs filtering to ModelID")
+        df = df[df['IsDefaultEntryForModel'] == 'Yes']
+        df = df.set_index('ModelID')
+        df = df.drop(columns=columns_to_drop).copy()
+    return (df)
