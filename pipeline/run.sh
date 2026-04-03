b81999ec4041c2bf3ef4f96cbef34b775bdc3357
Philip Montgomery <pmontgom@broadinstitute.org>
2026-04-03 10:24:14 -0400

    introduced run.sh

---
+++ pipeline/run.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+# Exit immediately if a command exits with a non-zero status
+set -ex
+
+# This script is intended to a very thin shell which is responsible for 
+# getting the environment set up enough to be able to run some command
+# within the poetry environment. It guarentees:
+#  - we're using the right python with the right dependencies
+#  - the depmap-deploy-repo is checked out into the right place
+#  - our current working directory is the 'pipeline' directory
+
+if [ "$DEPMAP_DEPLOY_BRANCH" == "" ];
+    echo "the environment variable DEPMAP_DEPLOY_BRANCH must be set to run this script"
+    exit 1
+fi
+
+# first start by figuring out where this run.sh script is so we can make all paths relative to that
+SCRIPT_HOME="$(cd "$(dirname "$(realpath "${BASH_SOURCE[0]}")")" && pwd)"
+
+# ==============================================
+# CHECKOUT DEPLOY REPO
+# ==============================================
+echo "Setting up depmap-deploy repo..."
+cd "$SCRIPT_HOME/.."
+if [ -d depmap-deploy ] ; then
+    ssh-agent bash -c 'ssh-add /home/ubuntu/.ssh/depmap-deploy-repo-key; cd depmap-deploy ; git pull'
+else
+    ssh-agent bash -c 'ssh-add /home/ubuntu/.ssh/depmap-deploy-repo-key; git clone git@github.com:broadinstitute/depmap-deploy.git'
+fi
+
+( cd depmap-deploy && git checkout "$DEPMAP_DEPLOY_BRANCH" )
+
+# Execute the remainder as shell command.
+cd "$SCRIPT_HOME"
+poetry "$@"
