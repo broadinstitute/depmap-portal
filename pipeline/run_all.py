7bafb6a417f64679b95d581768ada5da0b26f144
Philip Montgomery <pmontgom@broadinstitute.org>
2026-04-03 15:08:04 -0400

    added run_all script

---
+++ pipeline/run_all.py
@@ -0,0 +1,18 @@
+import subprocess
+import sys
+
+
+def run(cmd):
+    print(f"Executing {' '.join(args)}")
+    subprocess.run(cmd, check=True)
+
+
+# run the two pipelines passing all args to them
+run([sys.executable, "data-prep-pipeline/data_prep_pipeline_runner.py", sys.argv[1:]])
+run(
+    [
+        sys.executable,
+        "preprocessing-pipeline/preprocessing_pipeline_runner.py",
+        sys.argv[1:],
+    ]
+)
