6545b4b8f1f63825e1875c83a92187afece88247
Philip Montgomery <pmontgom@broadinstitute.org>
2026-04-15 15:39:38 -0400

    improvements to including files in preprocess taiga IDs

--- pipeline/preprocess_taiga_ids.py
+++ pipeline/preprocess_taiga_ids.py
@@ -33,17 +33,32 @@ def _resolve_versioned_dataset_id(taiga_permaname):
     return latest_cache[taiga_permaname]
 
 
-def _rewrite_stream(vars, in_name, in_lines, out_fd):
+def _rewrite_stream(vars, in_name, in_lines, out_fd, search_ancestors):
     errors = []
     fd = out_fd
     for line in in_lines:
         m = re.match('#\\s*TAIGA_PREPROCESSOR_INCLUDE\\s+"([^"]+)"\\s*', line)
         if m is not None:
             filename = m.group(1)
-            filename = os.path.join(os.path.dirname(in_name), filename)
+            if search_ancestors:
+                filename = _search_ancestors(filename, os.path.dirname(in_name))
+            else:
+                filename = os.path.join(os.path.dirname(in_name), filename)
+                if not os.path.exists(filename):
+                    raise UserError(
+                        f"Could not open {filename} which was included from {in_name}"
+                    )
+
             with open(filename, "rt") as fd_in:
                 included_lines = fd_in.readlines()
-                _rewrite_stream(vars, filename, included_lines, fd)
+                try:
+                    _rewrite_stream(
+                        vars, filename, included_lines, fd, search_ancestors
+                    )
+                except UserError as user_error:
+                    raise UserError(
+                        f"Could not process {filename} (included from {in_name}) due to error: \n  {user_error.message}"
+                    )
             continue
 
         m = re.match('#\\s*SET_TAIGA_PREPROCESSOR\\s+(\\S+)\\s+"([^"]+)"\\s*', line)
@@ -115,29 +130,35 @@ def _rewrite_stream(vars, in_name, in_lines, out_fd):
         )
 
 
-def rewrite_file(in_name, out_name):
+def rewrite_file(in_name, out_name, search_ancestors):
     with open(in_name, "rt") as fd:
         lines = fd.readlines()
 
     vars = {}
     with open(out_name, "wt") as out_fd:
-        _rewrite_stream(vars, in_name, lines, out_fd)
+        _rewrite_stream(vars, in_name, lines, out_fd, search_ancestors)
 
 
-def search_ancestors(filename):
-    ancestor_dir = os.getcwd()
+def _search_ancestors(filename, starting_point=None):
+    starting_points = []
+
+    if starting_point is not None:
+        starting_points.append(starting_point)
+    starting_points.append(os.getcwd())
+
     checked_paths = []
-    while True:
-        possible_path = os.path.join(ancestor_dir, filename)
-        checked_paths.append(possible_path)
-        if os.path.exists(possible_path):
-            return possible_path
-        next_dir = os.path.dirname(ancestor_dir)
-        if next_dir == ancestor_dir:
-            raise Exception(
-                f"Could not find {filename} (checked {', '.join(checked_paths)})"
-            )
-        ancestor_dir = next_dir
+    for ancestor_dir in starting_points:
+        while True:
+            possible_path = os.path.join(ancestor_dir, filename)
+            checked_paths.append(possible_path)
+            if os.path.exists(possible_path):
+                return possible_path
+            next_dir = os.path.dirname(ancestor_dir)
+            if next_dir == ancestor_dir:
+                break
+            ancestor_dir = next_dir
+    paths = "\n".join(["   " + x for x in checked_paths])
+    raise UserError(f"Could not find {filename} (checked :\n{paths})")
 
 
 if __name__ == "__main__":
@@ -154,10 +175,10 @@ if __name__ == "__main__":
     dst_file = arg.dst_file
     src_file = arg.src_file
     if arg.search_ancestors:
-        src_file = search_ancestors(src_file)
+        src_file = _search_ancestors(src_file)
 
     try:
-        rewrite_file(src_file, dst_file)
+        rewrite_file(src_file, dst_file, arg.search_ancestors)
     except UserError as err:
-        log.error(err.message)
+        log.error(f'Got error when prococessing "{src_file}": {err.message}')
         sys.exit(1)
