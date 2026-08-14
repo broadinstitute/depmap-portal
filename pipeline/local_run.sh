09f8ed6fea5958af28311f1d32bcfe70e42bd4d8
Philip Montgomery <pmontgom@broadinstitute.org>
2026-08-14 14:52:23 -0400

    moved all tempates to depmap-deploy

---
+++ pipeline/local_run.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+set -e
+
+if [[ -z "$2" || -z "$1" ]]; then
+    echo "Error: Environment name and pipeline name is required"
+    echo "Usage: $0 [analysis-pipeline | data-prep-pipeline] [internal|external|dmc]"
+    exit 1
+fi
+
+PIPELINE_NAME="$1"
+shift
+
+ENV_NAME="$1"
+shift
+
+if [[ ! -e ../../depmap-deploy ]]; then
+    echo "Expected depmap-deploy checked out at ../../depmap-deploy"
+    exit 1
+fi
+
+# Validate input
+
+if [[ "$ENV_NAME" != "internal" && "$ENV_NAME" != "external" && "$ENV_NAME" != "dmc" ]]; then
+    echo "Error: Parameter must be 'internal', 'external', or 'dmc'"
+    echo "Usage: $0 [internal|external|dmc]"
+    exit 1
+fi
+
+cd "$PIPELINE_NAME"
+
+echo "Preprocessing pipeline: Running conseq..."
+exec conseq run run_${ENV_NAME}.conseq "$@"
